eceb8ecc94b3dc6ddef9e28649629accad6a7942
max <iosyufeng@sina.com>
2018-08-31 09:32:09 +0800

    增加登陆页面

---
+++ src/pages/login/page/LoginPage.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react'
+import {
+    View,
+    Text,
+    TouchableOpacity,
+} from 'react-native'
+
+
+export default class LoginPage extends Component<Props>{
+    render(){
+        return(
+            <View>
+                   <TouchableOpacity onPress={this.back}>
+                        <Text>
+                            返回
+                        </Text>
+                    <TouchableOpacity/>
+            </View>
+        )
+    }
+
+        back()=()=>{
+                   this.props.navigation.goBack();
+                   }
+
+
+}
