038b7580e91086bcf70ef0205599a1e0fb612023
xiangchen <826464423@qq.com>
2018-08-31 13:40:43 +0800

    chore:视频播放控件

--- android/app/src/main/java/com/crm_app_xiugou/MainApplication.java
+++ android/app/src/main/java/com/crm_app_xiugou/MainApplication.java
@@ -3,6 +3,11 @@ package com.crm_app_xiugou;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.iou90.autoheightwebview.AutoHeightWebViewPackage;
+import com.brentvatne.react.ReactVideoPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.horcrux.svg.SvgPackage;
+import com.imagepicker.ImagePickerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -22,7 +27,12 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new AutoHeightWebViewPackage(),
+            new ReactVideoPackage(),
+            new VectorIconsPackage(),
+            new SvgPackage(),
+            new ImagePickerPackage()
       );
     }
 
