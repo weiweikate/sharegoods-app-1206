833036a86e97f9ffde3294fd95c56e93b897c66b
xiangchen <826464423@qq.com>
2018-09-06 15:31:16 +0800

    style: 优惠券部分

--- src/pages/mine/page/coupons/CouponsPage.js
+++ src/pages/mine/page/coupons/CouponsPage.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import {
-    View, Text
+    View, StyleSheet
 } from 'react-native';
-
+import ScrollableTabView ,{ScrollableTabBar}from 'react-native-scrollable-tab-view';
+import ScreenUtils from '../../../../utils/ScreenUtils'
+import MyCouponsItems from './../../components/MyCouponsItems';
 export default class CouponsPage extends Component {
 
     constructor(props) {
         super(props);
+        this.state={
+            selectTab:10
+        }
     }
 
     static $PageOptions = {
@@ -18,9 +23,33 @@ export default class CouponsPage extends Component {
 
     render() {
         return (
-            <View style={{ flex: 1, backgroundColor: '#f7f7f7' }}>
-                <Text>Coupons</Text>
+            <View style={styles.container}>
+                <ScrollableTabView
+                    onChangeTab={(obj)=>{
+                        this.setState({selectTab:obj.i})
+                    }}
+                    style={{width:ScreenUtils.width,justifyContent:'center',marginTop:1,backgroundColor:'#f7f7f7'}}
+                    //进界面的时候打算进第几个
+                    initialPage={2}
+                    tabBarBackgroundColor='#fff'
+                    tabBarActiveTextColor='red'
+                    tabBarInactiveTextColor='#999999'
+                    locked={true}
+                    tabBarTextStyle={{fontSize:15}}
+                    tabBarUnderlineStyle={{backgroundColor: 'red',height:2}}
+                    renderTabBar={() => <ScrollableTabBar />}
+                >
+                    <MyCouponsItems  tabLabel={'未使用'} pageStatus={0} nav={this.navigate} selectTab={this.state.selectTab} isgiveup={false} />
+                    <MyCouponsItems  tabLabel={'已失效'} pageStatus={1} nav={this.navigate} selectTab={this.state.selectTab} isgiveup={false}/>
+                    <MyCouponsItems  tabLabel={'已使用'} pageStatus={2} nav={this.navigate} selectTab={this.state.selectTab}  isgiveup={false}/>
+                </ScrollableTabView>
             </View>
         );
     }
 }
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#f7f7f7'
+    }
+});
