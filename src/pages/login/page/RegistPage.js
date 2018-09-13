4872cc1acdab37b47639b67db9d173276f397494
jinhui.m <jinhui.m@meeruu.com>
2018-09-13 19:34:29 +0800

    feat: 新增用户表协议 （临时内容）

--- src/pages/login/page/RegistPage.js
+++ src/pages/login/page/RegistPage.js
@@ -59,7 +59,12 @@ export default class RegistPage extends BasePage {
                     <Text style={{ fontSize: 11, color: ColorUtil.Color_666666 }}>
                         阅读并接受
                     </Text>
-                    <TouchableOpacity>
+                    <TouchableOpacity onPress={() => {
+                        this.$navigate('HtmlPage', {
+                            title: '用户协议内容',
+                            uri: 'https://reg.163.com/agreement_mobile_ysbh_wap.shtml?v=20171127'
+                        });
+                    }}>
                         <Text style={{ color: ColorUtil.mainRedColor, fontSize: 11 }}>
                             《用户协议》
                         </Text>
