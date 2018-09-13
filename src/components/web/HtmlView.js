8f0da7520b0f4fc7daee14396f3fd69b78850e6b
jinhui.m <jinhui.m@meeruu.com>
2018-09-13 19:42:38 +0800

    fix:  补充html组件

---
+++ src/components/web/HtmlView.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import {
+    WebView,
+} from 'react-native';
+import BasePage from '../../BasePage';
+
+export default class RequestDetailPage extends BasePage {
+
+    // 页面配置
+    $navigationBarOptions = {
+        title: ''
+    };
+
+    constructor(props) {
+        super(props);
+        const params = this.props.navigation.state.params || {};
+        const { uri, title } = params;
+        this.state = {
+            title: title,
+            uri: uri,
+        };
+    }
+
+    componentDidMount() {
+        this.$NavigationBarResetTitle(this.state.title);
+    }
+
+    _render() {
+
+        return (
+            <WebView source={{ uri: this.state.uri }}
+                     javaScriptEnabled={true}
+                     domStorageEnabled={true}
+                     scalesPageToFit={true}>
+            </WebView>
+        );
+    }
+}
+
