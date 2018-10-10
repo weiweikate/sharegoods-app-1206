45f9880d4a478b7c72786b787d5a63fabae7380d
magi <magi@gmail.com>
2018-10-10 10:35:51 +0800

    eslint修改

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -3,16 +3,7 @@
  */
 
 import React, { Component } from 'react';
-import {
-    View,
-    TouchableHighlight,
-    StyleSheet,
-    Image,
-    FlatList,
-    TouchableOpacity,
-    Platform
-} from 'react-native';
-import RouterMap from '../../RouterMap';
+import { View, StyleSheet,  FlatList} from 'react-native';
 import ScreenUtils from '../../utils/ScreenUtils';
 import {observer} from 'mobx-react';
 import { homeType, HomeModule } from './Modules'
@@ -24,64 +15,8 @@ import HomeRecommendView from './HomeRecommendView'
 import HomeActivityView from './HomeActivityView'
 import HomeBannerView from './HomeBannerView'
 import HomeSubjectView from './HomeSubjectView'
-import LinearGradient from 'react-native-linear-gradient'
 
 const { px2dp, statusBarHeight } = ScreenUtils;
-const DemoList = [
-    {
-        title: '专题页面类型one',
-        uri: 'home/subject/SubjectPage',
-        params: {
-            columnNumber: 2,
-            subjectType: 1
-        }
-    },
-    {
-        title: '专题页面类型Two',
-        uri: 'home/subject/SubjectPage',
-        params: {
-            columnNumber: 3,
-            subjectType: 0
-        }
-    },
-    {
-        title: '专题页面类型Three',
-        uri: 'home/subject/SubOpenPrizePage'
-
-    },
-    {
-        title: '优惠券',
-        uri: 'mine/coupons/CouponsPage'
-    },
-    {
-        title: '用户协议',
-        uri: 'HtmlPage',
-        params: {
-            title: '用户协议内容',
-            uri: 'https://reg.163.com/agreement_mobile_ysbh_wap.shtml?v=20171127'
-        }
-    },
-    {
-        title: '登录页面',
-        uri: 'login/login/LoginPage'
-    },
-    {
-        title: '我的订单',
-        uri: 'order/order/MyOrderTestPage',
-        params: {
-            index: 0
-        }
-    },
-    {
-        title: '搜索页面',
-        uri: RouterMap.SearchPage
-    },
-    {
-        title: '分类搜索',
-        uri: 'home/search/CategorySearchPage'
-    }
-];
-
 
 const bannerHeight = px2dp(220)
 
@@ -140,35 +75,10 @@ export default class HomePage extends Component {
                     keyExtractor={this._keyExtractor.bind(this)}
                     onScroll={this._onScroll.bind(this)}
                 />
-                {/* <View style={styles.navBarBg} ref={e => this._refHeader = e} >
-                    <HomeSearchView navigation={this.props.navigation}/>
-                </View> */}
                 <View style={styles.navBarBg} ref={e => this._refHeader = e} />
                 <View style={styles.navBar}>
                     <HomeSearchView navigation={this.props.navigation}/>
                 </View>
-                {/* <LinearGradient colors={['#000000', 'transparent']}
-                                style={[styles.navBar, { paddingTop: Platform.OS === 'ios' ? 44 : this.state.androidStatusH },
-                                    {
-                                        height: this.headerH + 14,
-                                        opacity: 0.5
-                                    }]}/>
-                <View colors={['#000000', 'transparent']}
-                      style={[styles.navBar, { paddingTop: Platform.OS === 'ios' ? 44 : this.state.androidStatusH },
-                          {
-                              height: this.headerH,
-                              opacity: 0.8
-                          }]}>
-                    <Image source={require('./res/icons/logo.png')} style={styles.logo}/>
-                    <TouchableOpacity style={styles.searchBox} onPress={()=> this.props.navigation.navigate('home/search/SearchPage')}>
-                        <Image source={require('./res/icon_search.png')} style={styles.searchIcon}/>
-                        <View style={styles.inputText}/>
-                    </TouchableOpacity>
-                    <TouchableHighlight onPress={() => this.props.navigation.navigate('message/MessageCenterPage')}>
-                        <Image source={require('./res/icons/msg.png')} style={styles.scanIcon}/>
-                    </TouchableHighlight>
-                </View>  */}
-
             </View>
 
         );
@@ -211,6 +121,5 @@ const styles = StyleSheet.create({
         left: 0,
         right: 0,
         zIndex: 3
-    },
-    
+    }
 });
--- src/pages/home/HomeSubjectView.js
+++ src/pages/home/HomeSubjectView.js
@@ -80,4 +80,4 @@ const styles = StyleSheet.create({
         flex: 1,
         marginHorizontal: 15
     }
-});
+})
