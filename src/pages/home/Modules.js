d2970f292fd3ef8995dc34b2d35ab4a90c3b73e3
magi <magi@gmail.com>
2018-10-15 09:36:54 +0800

    修复首页下来刷新逻辑错误

--- src/pages/home/Modules.js
+++ src/pages/home/Modules.js
@@ -307,6 +307,7 @@ class HomeModule {
         try {
             this.isFetching = true
             const res = yield HomeApi.getGoodsInHome({page: this.page})
+            this.isFetching = false
             let list = res.data.data
             if (list.length <= 0) {
                 this.isEnd = true
@@ -329,7 +330,6 @@ class HomeModule {
                 }
             }
             this.homeList = [...this.homeList, ...home]
-            this.isFetching = false
             this.page++
         } catch (error) {
             console.log(error)
