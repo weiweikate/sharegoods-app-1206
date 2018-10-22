0ce8c61a89cdfa46c93bd2e263e29d56e4caf711
胡超_iOS <huchao@meeruu.com>
2018-10-22 10:04:26 +0800

    秀值页面

---
+++ src/pages/shareTask/components/ShareTaskResultAlert.js
@@ -0,0 +1,51 @@
+/**
+ *
+ * Copyright 2018 杭州飓热科技有限公司   版权所有
+ * Copyright 2018 JURE Group Holding Ltd. All Rights Reserved
+ *
+ * @flow
+ * @format
+ * Created by huchao on 2018/10/19.
+ *
+ */
+"use strict";
+
+import React from "react";
+
+import {
+  StyleSheet,
+  View,
+  TouchableOpacity
+} from "react-native";
+
+import {
+  UIText,
+  UIImage
+} from "../../../components/ui";
+
+export default class ShareTaskResultAlert extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this._bind();
+
+    this.state = {};
+  }
+
+  _bind() {
+
+  }
+
+  componentDidMount() {
+  }
+
+
+  render() {
+    return (
+
+    );
+  }
+}
+
+const styles = StyleSheet.create({});
