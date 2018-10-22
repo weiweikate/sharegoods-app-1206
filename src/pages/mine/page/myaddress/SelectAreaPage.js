63ae9a48443bec5363f8d3879fbd6b42bb3a5229
lymluck <894405031@qq.com>
2018-10-22 11:53:29 +0800

    fix：修复地址管理bug

--- src/pages/mine/page/myaddress/SelectAreaPage.js
+++ src/pages/mine/page/myaddress/SelectAreaPage.js
@@ -1,16 +1,16 @@
-import BasePage from "../../../../BasePage";
-import { FlatList, Image, StyleSheet, TouchableOpacity, View } from "react-native";
-import React from "react";
-import UIText from "../../../../components/ui/UIText";
-import arrow_right from "../../../mine/res/customerservice/icon_06-03.png";
-import MineAPI from "../../api/MineApi";
-import bridge from "../../../../utils/bridge";
+import BasePage from '../../../../BasePage';
+import { FlatList, Image, StyleSheet, TouchableOpacity, View } from 'react-native';
+import React from 'react';
+import UIText from '../../../../components/ui/UIText';
+import arrow_right from '../../../mine/res/customerservice/icon_06-03.png';
+import MineAPI from '../../api/MineApi';
+import bridge from '../../../../utils/bridge';
 
 export default class SelectAreaPage extends BasePage {
 
     // 导航配置
     $navigationBarOptions = {
-        title: "选择地区"
+        title: '选择地区'
     };
 
     // 构造
@@ -48,13 +48,10 @@ export default class SelectAreaPage extends BasePage {
                     extraData={this.state}
                     onRefresh={this.refreshing}
                     refreshing={false}
-                    keyExtractor={(item) => item.id + ""}
+                    keyExtractor={(item) => item.id + ''}
                     showsVerticalScrollIndicator={false}
-                    getItemLayout={(data, index) => (
-                        //行高于分割线高，优化
-                        { length: 48, offset: (48 + 0.5) * index, index }
-                    )}
-                    data={this.state.datas} />
+                    initialNumToRender={15}
+                    data={this.state.datas}/>
             </View>
         );
     }
@@ -62,37 +59,37 @@ export default class SelectAreaPage extends BasePage {
     _renderItem = (item) => {
         return <TouchableOpacity style={styles.container} onPress={() => this._onItemClick(item.item)}>
             <UIText value={item.item.name} style={styles.blackText}/>
-            <Image source={arrow_right} style={{ width: 12, height: 20, marginRight: 18 }} resizeMode={"contain"}/>
+            <Image source={arrow_right} style={{ width: 12, height: 20, marginRight: 18 }} resizeMode={'contain'}/>
         </TouchableOpacity>;
     };
 
     _separator = () => {
-        return <View style={{ height: 0.5, backgroundColor: "#EEEEEE" }}/>;
+        return <View style={{ height: 0.5, backgroundColor: '#EEEEEE' }}/>;
     };
 
     _onItemClick = (item) => {
         const { setArea } = this.props.navigation.state.params || {};
-        if (this.state.tag === "province") {
+        if (this.state.tag === 'province') {
             // 跳转到市级
-            this.$navigate("mine/address/SelectAreaPage", {
+            this.$navigate('mine/address/SelectAreaPage', {
                 setArea: setArea,
-                tag: "city",
+                tag: 'city',
                 provinceCode: item.code,
                 provinceName: item.name,
                 fatherCode: item.code
             });
-        } else if (this.state.tag === "city") {
+        } else if (this.state.tag === 'city') {
             // 跳转到区级
-            this.$navigate("mine/address/SelectAreaPage", {
+            this.$navigate('mine/address/SelectAreaPage', {
                 setArea: setArea,
-                tag: "area",
+                tag: 'area',
                 provinceCode: this.props.navigation.state.params.provinceCode,
                 provinceName: this.props.navigation.state.params.provinceName,
                 cityCode: item.code,
                 cityName: item.name,
                 fatherCode: item.code
             });
-        } else if (this.state.tag === "area") {
+        } else if (this.state.tag === 'area') {
             // 回退并刷新
             this.$navigateBack(-3);
             const { provinceCode, provinceName, cityCode, cityName } = this.props.navigation.state.params || {};
@@ -105,15 +102,15 @@ export default class SelectAreaPage extends BasePage {
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: "white",
-        flexDirection: "row",
-        alignItems: "center",
+        backgroundColor: 'white',
+        flexDirection: 'row',
+        alignItems: 'center',
         height: 48
     },
     blackText: {
-        fontFamily: "PingFang-SC-Medium",
+        fontFamily: 'PingFang-SC-Medium',
         fontSize: 15,
-        color: "#222222",
+        color: '#222222',
         marginLeft: 15,
         flex: 1
     }
