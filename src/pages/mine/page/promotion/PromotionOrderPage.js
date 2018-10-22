0cb54bec68582234d3831b8e510a3e74d7914a20
xzm <2546308812@qq.com>
2018-10-22 11:56:26 +0800

    推广UI

--- src/pages/mine/page/promotion/PromotionOrderPage.js
+++ src/pages/mine/page/promotion/PromotionOrderPage.js
@@ -11,45 +11,97 @@
 "use strict";
 import React from "react";
 import {
-  StyleSheet,
-  View
+    StyleSheet,
+    View,
+    Text,
+    Image
 } from "react-native";
-import BasePage from "../../../BasePage";
-
+import BasePage from "../../../../BasePage";
+import ScreenUtils from "../../../../utils/ScreenUtils";
+const { px2dp } = ScreenUtils;
+import icon from './res/tongyon_icon_chenggong_nor.png'
 type Props = {};
 export default class PromotionOrderPage extends BasePage<Props> {
-  constructor(props) {
-    super(props);
-    this.state = {};
-    this._bind();
-  }
+    constructor(props) {
+        super(props);
+        this.state = {};
+        this._bind();
+    }
 
-  $navigationBarOptions = {
-    title: "",
-    show: true// false则隐藏导航
-  };
+    $navigationBarOptions = {
+        title: "",
+        show: true// false则隐藏导航
+    };
 
-  _bind() {
-    this.loadPageData = this.loadPageData.bind(this);
-  }
+    _bind() {
+        this.loadPageData = this.loadPageData.bind(this);
+    }
 
-  componentDidMount() {
-    this.loadPageData();
-  }
+    componentDidMount() {
+        this.loadPageData();
+    }
 
-  loadPageData() {
-  }
+    loadPageData() {
+    }
 
-  _render() {
-    return (
-      <View style={styles.container}>
-      </View>
-    );
-  }
+    _render() {
+        return (
+            <View style={styles.container}>
+                <Image source={icon} style={{width:px2dp(70),height:px2dp(70),marginTop:px2dp(66)}}/>
+                <Text style={{color:'#666666',fontSize:px2dp(15),marginTop:px2dp(15),marginBottom:px2dp(20)}}>
+                    支付成功
+                </Text>
+                <Text style={[styles.grayTextStyle,{marginBottom:px2dp(5)}]}>
+                    系统会在明天0点进行站内推广
+                </Text>
+                <Text style={styles.grayTextStyle}>
+                    每成功获取一个下级将收到站内消息推送
+                </Text>
+                <View style={styles.buttonsWrapper}>
+                    <View style={styles.buttonWrapper}>
+                        <Text style={styles.buttonTextStyle}>
+                            我的推广
+                        </Text>
+                    </View>
+                    <View style={{width:px2dp(10)}}/>
+                    <View style={styles.buttonWrapper}>
+                        <Text style={styles.buttonTextStyle}>
+                            站外分享推广
+                        </Text>
+                    </View>
+                </View>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1
-  }
+    container: {
+        flex: 1,
+        backgroundColor: "#f6f6f6",
+        alignItems:'center'
+    },
+    grayTextStyle:{
+        color:'#666666',
+        fontSize:px2dp(12)
+    },
+    buttonsWrapper:{
+        flexDirection:'row',
+        justifyContent:'space-between',
+        paddingHorizontal:px2dp(33),
+        marginTop:px2dp(30),
+        height:px2dp(48)
+    },
+    buttonWrapper:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center',
+        borderRadius:px2dp(5),
+        borderColor:'#D51243',
+        borderWidth:px2dp(0.5),
+    },
+    buttonTextStyle:{
+        color:'#D51243',
+        fontSize:px2dp(16)
+    }
 });
--- src/pages/mine/page/promotion/PromotionPayPage.js
+++ src/pages/mine/page/promotion/PromotionPayPage.js
@@ -8,48 +8,105 @@
  * Created by xzm on 2018/10/22.
  *
  */
+
 "use strict";
 import React from "react";
 import {
-  StyleSheet,
-  View
+    StyleSheet,
+    View,
+    TouchableOpacity,
+    Image
 } from "react-native";
-import BasePage from "../../../BasePage";
+import BasePage from "../../../../BasePage";
+import ScreenUtils from "../../../../utils/ScreenUtils";
+const { px2dp } = ScreenUtils;
+import UIText from "../../../../comm/components/UIText";
+import icon from './res/selected.png'
 
 type Props = {};
 export default class PromotionPayPage extends BasePage<Props> {
-  constructor(props) {
-    super(props);
-    this.state = {};
-    this._bind();
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            isSelected:false
+        };
+        this._bind();
+    }
 
-  $navigationBarOptions = {
-    title: "",
-    show: true// false则隐藏导航
-  };
+    $navigationBarOptions = {
+        title: "邀请推广",
+        show: true// false则隐藏导航
+    };
 
-  _bind() {
-    this.loadPageData = this.loadPageData.bind(this);
-  }
+    _bind() {
+        this.loadPageData = this.loadPageData.bind(this);
+    }
 
-  componentDidMount() {
-    this.loadPageData();
-  }
+    componentDidMount() {
+        this.loadPageData();
+    }
 
-  loadPageData() {
-  }
+    loadPageData() {
+    }
 
-  _render() {
-    return (
-      <View style={styles.container}>
-      </View>
-    );
-  }
+    _render() {
+        let unSelectView = (
+            <View style={{backgroundColor:'white',borderColor:'#DDDDDD',borderWidth:px2dp(0.5),width:px2dp(22),height:px2dp(22),borderRadius:px2dp(11)}}/>
+        );
+        let isSelectedView = (
+            <Image source={icon} style={{backgroundColor:'white',width:px2dp(22),height:px2dp(22)}}/>
+        );
+
+        return (
+            <View style={styles.container}>
+                <View style={{justifyContent:'center',paddingHorizontal:px2dp(15),height:px2dp(36)}}>
+                    <UIText style={{color:'#999999',fontSize:px2dp(12)}} value={'固定金额红包'}/>
+                </View>
+                <View style={styles.itemWrapper}>
+                    <UIText value={`红包金额：2元`}/>
+                    <TouchableOpacity onPress={()=>{
+                        this.setState({isSelected : !this.state.isSelected})
+                    }}>
+                    {this.state.isSelected ? isSelectedView : unSelectView}
+                    </TouchableOpacity>
+                </View>
+                <View style={{flex:1}}/>
+                <TouchableOpacity onPress={()=>alert()} disabled={!this.state.isSelected} >
+                    <View style={[styles.buttonWrapper,this.state.isSelected ? { opacity: 1} : {opacity : 0.5}]}>
+                        <UIText value={'提交支付'} style={{color:'white', fontSize: px2dp(16)}}/>
+                    </View>
+                </TouchableOpacity>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1
-  }
+    container: {
+        flex: 1,
+        backgroundColor: "#f6f6f6"
+    },
+    itemWrapper:{
+        height:px2dp(48),
+        width:ScreenUtils.width,
+        flexDirection:'row',
+        justifyContent:'space-between',
+        alignItems:'center',
+        paddingHorizontal:px2dp(15),
+        borderTopColor:'#DDDDDD',
+        borderBottomColor:'#DDDDDD',
+        borderBottomWidth:px2dp(0.5),
+        borderTopWidth:px2dp(0.5),
+        backgroundColor:'white'
+    },
+    buttonWrapper:{
+        height:px2dp(48),
+        width:ScreenUtils.width - px2dp(30),
+        alignSelf:'center',
+        borderRadius:px2dp(5),
+        backgroundColor:'#D51243',
+        justifyContent:'center',
+        alignItems:'center',
+        marginBottom:px2dp(15)
+    }
 });
