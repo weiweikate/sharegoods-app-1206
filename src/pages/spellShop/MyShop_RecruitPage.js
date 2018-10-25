9f1736de831bf25071bbb4cbfb250ede78cd89c0
slardar_chen <chen_yangjun@yeah.net>
2018-10-25 13:05:29 +0800

    fix: 增加关闭招募中店铺

--- src/pages/spellShop/MyShop_RecruitPage.js
+++ src/pages/spellShop/MyShop_RecruitPage.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import {
     View,
-    StyleSheet
+    StyleSheet,
+    ScrollView,
+    Image,
+    RefreshControl
 } from 'react-native';
 
 import BasePage from '../../BasePage';
@@ -11,8 +14,12 @@ import MyShopPage from './myShop/MyShopPage';
 import ShopRecruitPage from './shopRecruit/ShopRecruitPage';
 import RecommendPage from './recommendSearch/RecommendPage';
 import SpellShopApi from './api/SpellShopApi';
-import StringUtils from '../../utils/StringUtils';
 import { PageLoadingState } from '../../components/pageDecorator/PageState';
+import spellStatusModel from './model/SpellStatusModel';
+import IntroduceImg from './src/hhk_03.png';
+import IntroduceImg1 from './src/hhk_031.png';
+import NavigatorBar from '../../components/pageDecorator/NavigatorBar/NavigatorBar';
+import ScreenUtils from '../../utils/ScreenUtils';
 
 @observer
 export default class MyShop_RecruitPage extends BasePage {
@@ -22,7 +29,7 @@ export default class MyShop_RecruitPage extends BasePage {
         this.state = {
             loadingState: PageLoadingState.loading,
             netFailedInfo: {},
-            status: null
+            data: {}
         };
     }
 
@@ -47,38 +54,68 @@ export default class MyShop_RecruitPage extends BasePage {
     }
 
     _loadPageData = () => {
-        //店铺信息   搜索和推荐过来的this.params.storeId  首页直接展示的this.props.storeId
-        SpellShopApi.getById({ id: this.params.storeId || this.props.storeId }).then((data) => {
+        //店铺信息   首页搜索和推荐过来的this.params.storeId
+        SpellShopApi.getById({ id: this.params.storeId }).then((data) => {
+            this.state.login = true;
             let dataTemp = data.data || {};
             this.setState({
                 loadingState: PageLoadingState.success,
-                status: dataTemp.status
+                data: dataTemp
             });
         }).catch((error) => {
+            if (error.code === 10009) {
+                this.state.login = false;
+            } else {
+                this.state.login = true;
+            }
             this.setState({
-                loadingState: PageLoadingState.fail,
+                loadingState: error.code === 10009 ? PageLoadingState.success : PageLoadingState.fail,
                 netFailedInfo: error
             });
         });
     };
 
     _renderContainer = () => {
-        if (StringUtils.isEmpty(this.state.status)) {
-            return null;
-        }
-        if (this.state.status === 2) {
-            return (<RecommendPage navigation={this.props.navigation} leftNavItemHidden={true}/>);
-        } else if (this.state.status === 3) {
-            return <ShopRecruitPage navigation={this.props.navigation}
-                                    storeId={this.props.storeId}
-                                    leftNavItemHidden={this.props.leftNavItemHidden}
-                                    propReload={this.props.isHome && this._loadPageData}/>;
+        const { status, myStore } = this.state.data;
+        if (spellStatusModel.canSeeGroupStore && this.state.login) {
+            //首页搜索和推荐过来的this.params.storeId
+            switch (status) {
+                case 0:
+                case 1://店铺开启中
+                    return <MyShopPage navigation={this.props.navigation}
+                                       leftNavItemHidden={!this.params.storeId}
+                                       storeId={this.params.storeId}
+                                       propReload={myStore && this._loadPageData}/>;
+                case 3://招募中的店铺
+                    return <ShopRecruitPage navigation={this.props.navigation}
+                                            leftNavItemHidden={!this.params.storeId}
+                                            storeId={this.props.storeId}
+                                            propReload={myStore && this._loadPageData}/>;
+                case 2://店铺已缴纳保证金
+                    return (
+                        <RecommendPage navigation={this.props.navigation} leftNavItemHidden={!this.params.storeId}/>);
+                default:
+                    return (
+                        <RecommendPage navigation={this.props.navigation} leftNavItemHidden={!this.params.storeId}/>);
+
+            }
         } else {
-            return <MyShopPage navigation={this.props.navigation}
-                               storeId={this.props.storeId}
-                               leftNavItemHidden={this.props.leftNavItemHidden}
-                               propReload={this.props.isHome && this._loadPageData}/>;
+            return <View>
+                <NavigatorBar title={'拼店'} leftPressed={() => {
+                    this.$navigateBack();
+                }
+                }/>
+                <ScrollView refreshControl={<RefreshControl refreshing={spellStatusModel.refreshing}
+                                                            onRefresh={this._onRefresh}/>}
+                            showsVerticalScrollIndicator={false}>
+                    <View style={{ flex: 1 }}>
+                        <Image style={styles.levelLow} source={IntroduceImg} resizeMode='stretch'/>
+                        <Image style={styles.levelLow1} source={IntroduceImg1} resizeMode='stretch'/>
+                    </View>
+                </ScrollView>
+            </View>;
         }
+
     };
 
     _render() {
@@ -94,6 +131,17 @@ export default class MyShop_RecruitPage extends BasePage {
 const styles = StyleSheet.create({
     container: {
         flex: 1
+    },
+    levelLow: {
+        width: ScreenUtils.width,
+        height: ScreenUtils.height - ScreenUtils.headerHeight
+    },
+    levelLow1: {
+        position: 'absolute',
+        marginTop: ScreenUtils.isIOSX ? ScreenUtils.autoSizeHeight(78 + 60) : ScreenUtils.autoSizeHeight(78),
+        width: ScreenUtils.autoSizeWidth(375 - 59 * 2),
+        height: ScreenUtils.autoSizeWidth(257),
+        alignSelf: 'center'
     }
 });
 
