20c25ec8c804dcaeb097f063f4185ba2838a3141
胡超_iOS <huchao@meeruu.com>
2018-10-27 13:59:03 +0800

    选择图片压缩

---
+++ ios/Class/Modules/ImageCompression/XGImageCompression.h
@@ -0,0 +1,13 @@
+//
+//  XGImageCompression.h
+//  crm_app_xiugou
+//
+//  Created by 胡胡超 on 2018/10/27.
+//  Copyright © 2018年 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XGImageCompression : NSObject
+
+@end
---
+++ ios/Class/Modules/ImageCompression/XGImageCompression.m
@@ -0,0 +1,13 @@
+//
+//  XGImageCompression.m
+//  crm_app_xiugou
+//
+//  Created by 胡胡超 on 2018/10/27.
+//  Copyright © 2018年 Facebook. All rights reserved.
+//
+
+#import "XGImageCompression.h"
+
+@implementation XGImageCompression
+
+@end
