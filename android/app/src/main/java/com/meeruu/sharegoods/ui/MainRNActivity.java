106efe95793a50f244fa84af56994c801ae4064b
lymluck <894405031@qq.com>
2018-10-31 16:33:01 +0800

    fix

--- android/app/src/main/java/com/meeruu/sharegoods/ui/MainRNActivity.java
+++ android/app/src/main/java/com/meeruu/sharegoods/ui/MainRNActivity.java
@@ -340,7 +340,6 @@ public class MainRNActivity extends ReactActivity {
         }
     }
 
-
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
