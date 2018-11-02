33a202dca72d3a74e01a94427eeaadfd3390b7ac
max <iosyufeng@sina.com>
2018-11-02 18:13:47 +0800

    fix

--- ios/Class/Category/Delegate/AppDelegate+ConfigVC.m
+++ ios/Class/Category/Delegate/AppDelegate+ConfigVC.m
@@ -19,11 +19,11 @@
   
   //TODO: 应用根控制器的view指向了RCTRootView。但RCTRootView加载jsCodeLocation对应的js代码是耗时操作。
   //未了避免白屏，可以添加launchscreen一样的view强行覆盖window.等待js代码被真正执行时，再出发native移除此view.
-  #if DEBUG
+//  #if DEBUG
    NSURL *jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
-  #else
-   jsCodeLocation=[RCTHotUpdate bundleURL];
-  #endif
+//  #else
+//   jsCodeLocation=[RCTHotUpdate bundleURL];
+//  #endif
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"sharegoods"
                                                initialProperties:@{@"statusBarHeight":[NSNumber numberWithFloat:kStatusBarHeight]}
