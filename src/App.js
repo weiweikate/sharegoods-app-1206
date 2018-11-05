06dc73add5ac7d69f2af78ce434cd51a0cd1fb04
max <iosyufeng@sina.com>
2018-11-05 19:26:44 +0800

    对接热更新

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import {
     Text,
     View,
     Platform,
-    NativeModules
+    NativeModules, Alert, Linking
 } from 'react-native';
 import { NavigationActions, StackNavigator } from 'react-navigation';
 import CardStackStyleInterpolator from 'react-navigation/src/views/CardStack/CardStackStyleInterpolator';
@@ -24,10 +24,21 @@ import appData from './model/appData';
 import { netStatus } from './comm/components/NoNetHighComponent';
 import signTestTool from './signTestTool';
 
+import {
+    isFirstTime,
+    isRolledBack,
+    checkUpdate,
+    downloadUpdate,
+    switchVersion,
+    switchVersionLater,
+    markSuccess,
+} from 'react-native-update';
+
+import _updateConfig from '../update.json';
+const {appKey} = _updateConfig[Platform.OS];
 
 export default class App extends Component {
     constructor(props) {
-
         appData.setStatusBarHeight(props.statusBarHeight);
 
         super(props);
@@ -43,7 +54,50 @@ export default class App extends Component {
         await user.readUserInfoFromDisk();
         global.$navigator = this.refs.Navigator;
         global.$routes = [];
+
+        //热更新相关
+        if (isFirstTime) {
+            markSuccess()
+        } else if (isRolledBack) {
+            Alert.alert('提示', '刚刚更新失败了,版本被回滚.');
+        }
     }
+    componentDidMount(){
+        // this.checkUpdate()
+    }
+
+    doUpdate = info => {
+        downloadUpdate(info).then(hash => {
+            Alert.alert('提示', '下载完毕,是否重启应用?', [
+                {text: '是', onPress: ()=>{switchVersion(hash);}},
+                {text: '否',},
+                {text: '下次启动时', onPress: ()=>{switchVersionLater(hash);}},
+            ]);
+        }).catch(err => {
+            Alert.alert('提示', '更新失败.');
+        });
+    };
+    checkUpdate = () => {
+        checkUpdate(appKey).then(info => {
+            if (info.expired) {
+                Alert.alert('提示', '您的应用版本已更新,请前往应用商店下载新的版本', [
+                    {text: '确定', onPress: ()=>{info.downloadUrl && Linking.openURL(info.downloadUrl)}},
+                ]);
+            } else if (info.upToDate) {
+                /**
+                 * Alert.alert('提示', '您的应用版本已是最新.');
+                 * 如果已是罪行版本则什么也不需要做
+                 * */
+            } else {
+                Alert.alert('提示', '检查到新的版本'+info.name+',是否下载?\n'+ info.description, [
+                    {text: '是', onPress: ()=>{this.doUpdate(info)}},
+                    {text: '否',},
+                ]);
+            }
+        }).catch(err => {
+            Alert.alert('提示', '更新失败.');
+        });
+    };
 
     render() {
         const Navigator = StackNavigator(Router,
--- src/pages/shopCart/page/ShopCartPage.js
+++ src/pages/shopCart/page/ShopCartPage.js
@@ -50,7 +50,7 @@ const activityString = {
 export default class ShopCartPage extends BasePage {
     // 导航配置
     $navigationBarOptions = {
-        title: '购物车',
+        title: '购物车更改',
         leftNavItemHidden: true
 
     };
