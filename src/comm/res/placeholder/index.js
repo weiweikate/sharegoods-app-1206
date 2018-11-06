8b17ff7d57922673a6df77f3dd3a31a1cce970d6
胡超_iOS <huchao@meeruu.com>
2018-11-06 15:24:47 +0800

    1111

---
+++ src/comm/res/placeholder/index.js
@@ -0,0 +1,9 @@
+const placeholder = {
+    weiXin: require('./share_icon_wechat_circle.png'),
+    weiXinTimeLine: require('./share_icon_pengyouquan_nor.png'),
+    QQ: require('./share_icon_qq_nor.png'),
+    qqKongJian: require('./share_icon_kongjian_nor.png'),
+    download: require('./share_btn_download_nor.png'),
+    saveImage: require('./share_icon_baocun.png')
+};
+export default placeholder;
