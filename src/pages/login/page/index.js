242032fe54da8a0a464687909d296fb53a1df55b
max <iosyufeng@sina.com>
2018-11-06 20:21:03 +0800

    fix:修复领红包bug

--- src/pages/login/page/index.js
+++ src/pages/login/page/index.js
@@ -3,9 +3,9 @@ import OldUserLoginPage from './OldUserLoginPage';
 import SetPasswordPage from './SetPasswordPage';
 import RegistPage from './RegistPage';
 import ForgetPasswordPage from './ForgetPasswordPage';
-import GetRedpacketPage from './GetRedpacketPage'
-import TestNav from './testNav'
-import InviteCodePage from './InviteCodePage'
+import GetRedpacketPage from './GetRedpacketPage';
+import TestNav from './testNav';
+import InviteCodePage from './InviteCodePage';
 
 // 访问路径 'Login/Demo1'
 export default {
