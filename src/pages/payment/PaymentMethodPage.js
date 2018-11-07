2d99d557e659927107502c18f7f4390aacd5af0c
magi <magi@gmail.com>
2018-11-07 15:33:59 +0800

    修复礼包支付确认

--- src/pages/payment/PaymentMethodPage.js
+++ src/pages/payment/PaymentMethodPage.js
@@ -110,12 +110,19 @@ export default class PaymentMethodPage extends BasePage {
     _handleAppStateChange = (state) => {
         console.log('_handleAppStateChange AppState', state);
         const { selectedTypes } = this.payment;
+        let paytype = 1
+        if (this.payment.payStore) {
+            paytype = 2
+        }
+        if (this.payment.payPromotion) {
+            paytype = 3
+        }
         if (state === 'active' && this.payment.outTradeNo) {
             if (selectedTypes.type === paymentType.alipay) {
                 this.payment.alipayCheck({
                     outTradeNo: this.payment.outTradeNo,
                     type: paymentType.alipay,
-                    payType: 1
+                    payType: paytype
                 }).then(checkStr => {
                     console.log('_handleAppStateChange', state, checkStr);
                     this._showPayresult(checkStr.resultStr);
@@ -125,7 +132,7 @@ export default class PaymentMethodPage extends BasePage {
                 this.payment.wechatCheck({
                     outTradeNo: this.payment.outTradeNo,
                     type: 2,
-                    payType: 1
+                    payType: paytype
                 }).then(checkStr => {
                     console.log('_handleAppStateChange', state, checkStr);
                     this._showPayresult(checkStr.resultStr);
