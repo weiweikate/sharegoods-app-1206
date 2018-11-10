f8e4bc4e36869d64a42d9af175e963ef742c372d
胡超_iOS <huchao@meeruu.com>
2018-11-10 17:54:44 +0800

    iOSapp处理白屏

--- ios/Class/Category/UIView/UIImage+Util.h
+++ ios/Class/Category/UIView/UIImage+Util.h
@@ -11,7 +11,7 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface UIImage (Util)
-
++ (UIImage *)getLaunchImage;
 @end
 
 NS_ASSUME_NONNULL_END
--- ios/Class/Category/UIView/UIImage+Util.m
+++ ios/Class/Category/UIView/UIImage+Util.m
@@ -9,5 +9,21 @@
 #import "UIImage+Util.h"
 
 @implementation UIImage (Util)
++ (UIImage *)getLaunchImage{
+  
+  CGSize viewSize = [UIScreen mainScreen].bounds.size;
+  NSString *viewOr = @"Portrait";//垂直
+  NSString *launchImage = nil;
+  NSArray *launchImages =  [[[NSBundle mainBundle] infoDictionary] valueForKey:@"UILaunchImages"];
+  
+  for (NSDictionary *dict in launchImages) {
+    CGSize imageSize = CGSizeFromString(dict[@"UILaunchImageSize"]);
+    
+    if (CGSizeEqualToSize(viewSize, imageSize) && [viewOr isEqualToString:dict[@"UILaunchImageOrientation"]]) {
+      launchImage = dict[@"UILaunchImageName"];
+    }
+  }
+  return [UIImage imageNamed:launchImage];
+}
 
 @end
