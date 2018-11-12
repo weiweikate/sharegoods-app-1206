ce5213a1c1dc177a3ad6812ee148f3a3e0c246fd
magi <magi@gmail.com>
2018-11-12 16:56:49 +0800

    修复秀场轮播图片拉伸

--- src/components/ui/XGSwiper.js
+++ src/components/ui/XGSwiper.js
@@ -146,13 +146,13 @@ export default class XQSwiper extends Component {
         const { scaleArray, translateArray } = this.state;
         for (let i = 0; i < this.ezswiper.count + 2; i++) {
             if (i === scrollIndex) {
-                scaleArray[i].setValue(this.props.ratio);
+                scaleArray[i].setValue(1);
                 translateArray[i].setValue(this.ezswiper.cardParams.cardTranslate * (currentPageFloat - scrollIndex));
             } else if (i === scrollIndex - 1 || i === scrollIndex + 1) {
-                scaleArray[i].setValue(this.props.ratio);
+                scaleArray[i].setValue(1);
                 translateArray[i].setValue((currentPageFloat - i) * this.ezswiper.cardParams.cardTranslate);
             } else {
-                scaleArray[i].setValue(this.props.ratio);
+                scaleArray[i].setValue(1);
                 translateArray[i].setValue((currentPageFloat - i) * this.ezswiper.cardParams.cardTranslate);
             }
 
