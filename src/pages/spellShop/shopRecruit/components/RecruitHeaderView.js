3123283b5ed509de715cc48514a097ffc1c9d169
slardar_chen <chen_yangjun@yeah.net>
2018-11-13 10:20:25 +0800

    fix: 招募中页面适配效果

--- src/pages/spellShop/shopRecruit/components/RecruitHeaderView.js
+++ src/pages/spellShop/shopRecruit/components/RecruitHeaderView.js
@@ -50,7 +50,7 @@ export default class RecommendRow extends Component {
         return <View style={styles.bg}>
             <ImageBackground source={HeaderBarBgImg} style={styles.headerBg}>
                 <View style={{
-                    marginTop: 21 + ScreenUtils.headerHeight,
+                    marginTop: ScreenUtils.autoSizeWidth(21) + ScreenUtils.headerHeight,
                     flexDirection: 'row',
                     marginLeft: ScreenUtils.autoSizeWidth(57)
                 }}>
@@ -85,7 +85,7 @@ export default class RecommendRow extends Component {
                     <View style={{ backgroundColor: '#E4E4E4', height: 0.5 }}/>
                     <View style={{
                         flexDirection: 'row',
-                        height: 57,
+                        height: ScreenUtils.autoSizeWidth(57),
                         paddingHorizontal: 24,
                         alignItems: 'center',
                         justifyContent: 'space-between'
@@ -131,7 +131,7 @@ const styles = StyleSheet.create({
     },
     //白的面板背景
     whiteBg: {
-        marginTop: 30,
+        marginTop: ScreenUtils.autoSizeWidth(22.5),
         backgroundColor: DesignRule.white,
         marginHorizontal: 15,
         height: ScreenUtils.autoSizeWidth(144),
