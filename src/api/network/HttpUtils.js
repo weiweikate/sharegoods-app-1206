99ba8be790cecec0dd695e582b70683c25b89301
slardar_chen <chen_yangjun@yeah.net>
2018-11-16 10:26:30 +0800

    perf: 店铺公告页状态增加和加载更多增加   get请求参数编码  本地联调地址跟换

--- src/api/network/HttpUtils.js
+++ src/api/network/HttpUtils.js
@@ -80,6 +80,7 @@ export default class HttpUtils {
                 url = url + '?' + Qs.stringify(params);
             }
         }
+        url = decodeURIComponent(url);
 
         /**
          * @type {*|{nonce, timestamp, client, version, sign}}
