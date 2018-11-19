14de438aa9d03f58f8b2697b5c54c8eb483446c2
lymluck <894405031@qq.com>
2018-11-19 11:58:20 +0800

    fix：测试版

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -12,9 +12,9 @@ import {
 import ScreenUtils from '../../utils/ScreenUtils';
 import ShareTaskIcon from '../shareTask/components/ShareTaskIcon';
 import { observer } from 'mobx-react';
-import { homeModule } from './Modules'
-import { homeType } from './HomeTypes'
-import { bannerModule } from './HomeBannerModel'
+import { homeModule } from './Modules';
+import { homeType } from './HomeTypes';
+import { bannerModule } from './HomeBannerModel';
 import HomeSearchView from './HomeSearchView';
 import HomeClassifyView from './HomeClassifyView';
 import HomeStarShopView from './HomeStarShopView';
@@ -40,7 +40,8 @@ import DeviceInfo from 'react-native-device-info';
 import StringUtils from '../../utils/StringUtils';
 import DesignRule from 'DesignRule';
 import res from '../../comm/res';
-import TimerMixin from 'react-timer-mixin'
+import TimerMixin from 'react-timer-mixin';
+
 const closeImg = res.button.cancel_white_circle;
 /**
  * @author zhangjian
