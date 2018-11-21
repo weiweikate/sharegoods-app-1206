2ca351af275ab2702cf4e2b8ff5b2d244d0a6e9b
xzm <2546308812@qq.com>
2018-11-21 15:02:43 +0800

    首页弹窗弹出优先级调整

--- src/pages/home/model/HomeModalManager.js
+++ src/pages/home/model/HomeModalManager.js
@@ -31,22 +31,22 @@ class HomeModalManager {
     @action
     getVersion = () => {
         return MineApi.getVersion({ version: DeviceInfo.getVersion() }).then((resp) => {
-            this.setVersion(resp.data);
-            this.getMessage();
+            this.setVersion(resp);
             return resp;
         });
     };
 
+    @action
     getMessage() {
         var currStr = new Date().getTime() + "";
-        AsyncStorage.getItem("lastMessageTime").then((value) => {
+        return AsyncStorage.getItem("lastMessageTime").then((value) => {
             if (value == null || parseInt(currStr) - parseInt(value) > 24 * 60 * 60 * 1000) {
-                MessageApi.queryNotice({ page: this.currentPage, pageSize: 10, type: 100 }).then(resp => {
-
+                return MessageApi.queryNotice({ page: this.currentPage, pageSize: 10, type: 100 }).then(resp => {
+                    this.setHomeMessage(resp);
+                    return resp;
                 });
             }
         });
-        AsyncStorage.setItem("lastMessageTime", currStr);
     }
 }
 
