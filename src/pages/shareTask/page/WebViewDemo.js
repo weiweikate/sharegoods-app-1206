06f3810a4b4098cfec2ad8f8d147b2165a6ccbb1
胡超_iOS <huchao@meeruu.com>
2018-11-27 10:22:41 +0800

    添加h5交互测试入口

--- src/pages/shareTask/page/WebViewDemo.js
+++ src/pages/shareTask/page/WebViewDemo.js
@@ -10,13 +10,14 @@
  */
 'use strict';
 import React from 'react';
-import {
-  StyleSheet,
-  View
-} from 'react-native';
+// import {
+//   StyleSheet,
+//   View
+// } from 'react-native';
 import BasePage from '../../../BasePage';
-import DesignRule from 'DesignRule';
-import RouterMap from 'RouterMap';
+// import DesignRule from 'DesignRule';
+// import RouterMap from 'RouterMap';
+import WebViewBridge from '@mr/react-native-webview'
 
 
 export default class WebViewDemo extends BasePage {
@@ -27,7 +28,7 @@ export default class WebViewDemo extends BasePage {
   }
 
   $navigationBarOptions = {
-    title: '',
+    title:  '加载中...',
     show: true// false则隐藏导航
   };
 
@@ -42,12 +43,22 @@ export default class WebViewDemo extends BasePage {
   loadPageData() {
   }
 
-  _render() {
-    return (
-      <View style={DesignRule.style_container}>
-      </View>
-    );
-  }
+    _render(){
+        return(
+            <WebViewBridge
+                token={'Q/FHsj8FwJOOwXFVmjsx3ma7vMzNmfwkV9fb3ATP2wrUIEMJ8D1ljGozxckLWy2ESJkHzel6a3gasklC5Apw7//6tkTH3vD9jx3zwPvjR3qgVUJuoBE9UmK6Lzxe8ZH1pD8ThhwSMyqSw/Uv5TMRxg=='}
+                style={{flex: 1}}
+                // source={this.props.source} //uri: 'http://172.16.10.117:9528/topic/first'
+                source={{uri: 'http://172.16.10.117:9528/topic/first'}}
+                // postMessage={msg => this._postMessage(msg)}
+                navigateAppPage={(r, p) => {this.$navigate(r, p)}}
+                // onLoadStart={() => this._onLoadStart()}
+                // onLoadEnd={() => this._onLoadEnd()}
+                // onError={e => this._onError(e)}
+                onNavigationStateChange={event => event.title.length>0 && this.$NavigationBarResetTitle(event.title)}
+            />
+        )
+    }
 }
 
-const styles = StyleSheet.create({});
+// const styles = StyleSheet.create({});
