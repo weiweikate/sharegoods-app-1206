e6680e4f15056e593887ab1d0104d6b2ac79f097
xzm <2546308812@qq.com>
2018-11-28 20:48:03 +0800

    1.简介编辑view

--- src/pages/mine/page/MinePage.js
+++ src/pages/mine/page/MinePage.js
@@ -52,7 +52,8 @@ const {
     mine_setting_icon_white,
     arrowRight,
     profile_banner,
-    mine_level_background
+    mine_level_background,
+    mine_icon_mentor
 } = res.homeBaseImg;
 
 
@@ -729,8 +730,8 @@ export default class MinePage extends BasePage {
     }
 
     renderMenu = () => {
-        let leftImage = [mine_icon_invite, mine_coupon_icon, mine_icon_data, mine_icon_favorite_shop, mine_icon_help_service, mine_icon_address, mine_icon_notificate, mine_icon_mission, mine_icon_discollect, mine_icon_discollect];
-        let leftText = ["邀请好友", "优惠券", "我的晋升", "收藏店铺", "帮助与客服", "地址", "我的推广", "我的任务", "秀场收藏", "测试h5的交互"];
+        let leftImage = [mine_icon_invite, mine_coupon_icon, mine_icon_data, mine_icon_favorite_shop, mine_icon_help_service, mine_icon_address, mine_icon_notificate, mine_icon_mission, mine_icon_discollect, mine_icon_discollect,mine_icon_mentor];
+        let leftText = ["邀请好友", "优惠券", "我的晋升", "收藏店铺", "帮助与客服", "地址", "我的推广", "我的任务", "秀场收藏", "测试h5的交互",'导师'];
 
         let arr = [];
         for (let i = 0; i < leftImage.length; i++) {
---
+++ src/pages/mine/page/userInformation/ProfileEditPage.js
@@ -0,0 +1,132 @@
+/**
+ *
+ * Copyright 2018 杭州飓热科技有限公司   版权所有
+ * Copyright 2018 JuRe Group Holding Ltd. All Rights Reserved
+ *
+ * @flow
+ *
+ * Created by xzm on 2018/11/28.
+ *
+ */
+
+
+"use strict";
+import React from "react";
+import {
+    StyleSheet,
+    TouchableWithoutFeedback,
+    View,
+    Text,
+    TextInput
+} from "react-native";
+import BasePage from "../../../../BasePage";
+import DesignRule from "../../../../constants/DesignRule";
+import ScreenUtils from "../../../../utils/ScreenUtils";
+
+const { px2dp } = ScreenUtils;
+export default class ProfileEditPage extends BasePage {
+    constructor(props) {
+        super(props);
+        this.state = {
+            textNum : 0
+        };
+        this._bind();
+    }
+
+    $navigationBarOptions = {
+        title: "编辑简介",
+        show: true// false则隐藏导航
+    };
+
+    $NavBarRenderRightItem() {
+        return (
+            <TouchableWithoutFeedback onPress={this._commitProfile}>
+                <Text style={styles.navRightItemStyle}>
+                    完成
+                </Text>
+            </TouchableWithoutFeedback>
+        );
+    }
+
+    _bind() {
+        this.loadPageData = this.loadPageData.bind(this);
+    }
+
+    // _render(){
+    //     return<View style={{backgroundColor:'red',width:100,height:100}}/>
+    // }
+
+
+    componentDidMount() {
+        this.loadPageData();
+    }
+
+    loadPageData() {
+    }
+
+    _commitProfile() {
+
+    }
+
+    _render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.tipTextStyle}>
+                    请填写您的介绍
+                </Text>
+                <View>
+                    <TextInput style={styles.textInputStyle}
+                               maxLength={90}
+                               multiline={true}
+                               underlineColorAndroid={"transparent"}
+                               onChangeText={(text)=>{
+                                   if(text){
+                                       this.setState({
+                                           textNum:text.length
+                                       })
+                                   }else {
+                                       this.setState({
+                                           textNum:0
+                                       })
+                                   }
+                               }}
+                    />
+                    <Text style={styles.textLimitStyle}>
+                        {`${this.state.textNum}/90`}
+                    </Text>
+                </View>
+            </View>
+        );
+    }
+
+
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    navRightItemStyle: {
+        color: DesignRule.mainColor,
+        fontSize: DesignRule.fontSize_threeTitle
+    },
+    tipTextStyle: {
+        color: DesignRule.textColor_instruction,
+        fontSize: DesignRule.fontSize_24,
+        includeFontPadding: false,
+        marginVertical: px2dp(10),
+        marginLeft: DesignRule.margin_page
+    },
+    textInputStyle: {
+        backgroundColor: DesignRule.white,
+        height: px2dp(133),
+        width: ScreenUtils.width,
+        padding: DesignRule.margin_page,
+        textAlignVertical: "top"
+    },
+    textLimitStyle:{
+        position:'absolute',
+        bottom:DesignRule.margin_page,
+        right:DesignRule.margin_page
+    }
+});
