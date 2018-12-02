a06a9cc0a328d63114c7f9ffb5717cb8f825521b
slardar_chen <chen_yangjun@yeah.net>
2018-12-02 15:15:03 +0800

    fix: 我的页面修改我的客服

--- src/pages/mine/page/MinePage.js
+++ src/pages/mine/page/MinePage.js
@@ -731,7 +731,7 @@ export default class MinePage extends BasePage {
     renderMenu = () => {
 
         let leftImage = [mine_icon_invite, mine_coupon_icon, mine_icon_data, mine_icon_favorite_shop, mine_icon_help_service, mine_icon_address, mine_icon_mission, mine_icon_discollect, mine_icon_discollect, user.upUserid ? mine_icon_mentor : null];
-        let leftText = ["邀请好友", "优惠券", "我的晋升", "收藏店铺", "帮助与客服", "地址", "我的任务", "秀场收藏", "测试h5的交互", user.upUserid ? "导师" : null];
+        let leftText = ["邀请好友", "优惠券", "我的晋升", "收藏店铺", "我的客服", "地址", "我的任务", "秀场收藏", "测试h5的交互", user.upUserid ? "导师" : null];
 
         let arr = [];
         for (let i = 0; i < leftImage.length; i++) {
