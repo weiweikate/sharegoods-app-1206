731568800967fcc5c769c59239a9c92480ae142e
胡超_iOS <huchao@meeruu.com>
2018-12-18 10:57:49 +0800

    111

--- src/utils/SensorsTrack.js
+++ src/utils/SensorsTrack.js
@@ -1,10 +1,33 @@
 import { NativeModules } from 'react-native';
-const RNSensorsAnalyticsModule = NativeModules.RNSensorsAnalyticsModule;
+const {track, trackTimerStart, trackTimerEnd} = NativeModules.RNSensorsAnalyticsModule;
+// track("event_name",parmas)
+// trackTimerStart("event_name")
+// trackTimerEnd("event_name",parmas)
 
-const OrderTrack = {
-    example: '$example',
+/** 订单相关的埋点事件名称*/
+const trackEvent = {
+    bannerClick: 'bannerClick',//banner点击
+    login: 'login',//登录
+    signUp: 'signUp',//注册
+    search: 'search',//商品搜索
+    commodityDetail: 'commodityDetail',//浏览商品详情页
+    addToShoppingcart: 'addToShoppingcart',//加入购物车
+    submitOrder: 'submitOrder',//提交订单
+    submitOrderDetail: 'submitOrderDetail',//提交订单详情
+    payOrderpayOrder: 'payOrderpayOrder',//支付订单支付订单
+    payOrderDetail: 'payOrderDetail',//支付订单详情
+    cancelPayOrder: 'cancelPayOrder',//取消订单
+    applyReturn: 'applyReturn',//申请退货
+    receiveDiscountreceiveDiscount: 'receiveDiscountreceiveDiscount',//领取优惠券领取优惠券
+    share: 'share',//分享商品
+    contact: 'contact',//联系客服
+    QrcodeShareto: 'QrcodeShareto',//分享二维码
 };
 
-// RNSensorsAnalyticsModule.track("event_name",parmas:{})}>
-export default RNSensorsAnalyticsModule.track;
-export {OrderTrack};
+
+export {
+    track,
+    trackTimerStart,
+    trackTimerEnd,
+    trackEvent
+};
