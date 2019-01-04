77376903d7338793816a244a8f2f85bb946148f8
magi <magi@gmail.com>
2019-01-04 20:04:21 +0800

    秀场

--- src/pages/show/ShowApi.js
+++ src/pages/show/ShowApi.js
@@ -2,7 +2,7 @@ const api = {
     // 获取秀场
    showQuery: ['/discover/query', {method: 'get'}],
    // 发现详情
-   showDetail: ['/discover/getById', {method: 'get'}],
+   showDetail: ['/gateway/discover/getById', {method: 'get'}],
    // 发现详情
    showDetailCode: ['/discover/getByCode', {method: 'get'}],
    // 点赞
