675fe457423678cae4aa4f87d564ec5548b522df
slardar_chen <chen_yangjun@yeah.net>
2019-01-04 19:59:06 +0800

    perf: 颜色

--- src/components/ui/video/VideoView.js
+++ src/components/ui/video/VideoView.js
@@ -332,7 +332,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
-        backgroundColor: '#efefef'
+        backgroundColor: DesignRule.imgBg_color
     },
     playButton: {
         width: 50,
