8890b9d9b1c00d754a1276fbd10469ecea68f2c9
slardar_chen <chen_yangjun@yeah.net>
2019-01-04 19:43:24 +0800

    perf: 视频背景颜色跟图片一致

--- src/components/ui/video/VideoView.js
+++ src/components/ui/video/VideoView.js
@@ -332,7 +332,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
-        backgroundColor: DesignRule.textColor_mainTitle
+        backgroundColor: '#efefef'
     },
     playButton: {
         width: 50,
