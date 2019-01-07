01a4c52f3d1baa9aa10a0e8f385430eaed53bae3
lymluck <894405031@qq.com>
2019-01-07 19:25:44 +0800

    fix：修复jira上的bug

--- src/pages/mine/page/myaddress/SelectAreaPage.js
+++ src/pages/mine/page/myaddress/SelectAreaPage.js
@@ -1,18 +1,19 @@
-import BasePage from "../../../../BasePage";
-import { FlatList, Image, RefreshControl, StyleSheet, TouchableOpacity, View } from "react-native";
-import React from "react";
-import UIText from "../../../../components/ui/UIText";
-import MineAPI from "../../api/MineApi";
-import bridge from "../../../../utils/bridge";
+import BasePage from '../../../../BasePage';
+import { FlatList, Image, RefreshControl, StyleSheet, View } from 'react-native';
+import React from 'react';
+import UIText from '../../../../components/ui/UIText';
+import MineAPI from '../../api/MineApi';
+import bridge from '../../../../utils/bridge';
 import DesignRule from '../../../../constants/DesignRule';
-import res from "../../res";
+import res from '../../res';
+import NoMoreClick from '../../../../components/ui/NoMoreClick';
 
 const arrow_right = res.button.arrow_right;
 export default class SelectAreaPage extends BasePage {
 
     // 导航配置
     $navigationBarOptions = {
-        title: "选择地区"
+        title: '选择地区'
     };
 
     // 构造
@@ -23,6 +24,7 @@ export default class SelectAreaPage extends BasePage {
             tag: props.navigation.state.params.tag,
             fatherCode: props.navigation.state.params.fatherCode
         };
+        this.canBack = true;
     }
 
     $isMonitorNetworkStatus() {
@@ -57,7 +59,7 @@ export default class SelectAreaPage extends BasePage {
                     ItemSeparatorComponent={this._separator}
                     renderItem={this._renderItem}
                     extraData={this.state}
-                    keyExtractor={(item) => item.id + ""}
+                    keyExtractor={(item) => item.id + ''}
                     showsVerticalScrollIndicator={false}
                     initialNumToRender={15}
                     data={this.state.datas}
@@ -70,10 +72,10 @@ export default class SelectAreaPage extends BasePage {
     }
 
     _renderItem = (item) => {
-        return <TouchableOpacity style={styles.container} onPress={() => this._onItemClick(item.item)}>
+        return <NoMoreClick style={styles.container} onPress={() => this._onItemClick(item.item)}>
             <UIText value={item.item.name} style={styles.blackText}/>
-            <Image source={arrow_right} style={{ marginRight: 18 }} resizeMode={"contain"}/>
-        </TouchableOpacity>;
+            <Image source={arrow_right} style={{ marginRight: 18 }} resizeMode={'contain'}/>
+        </NoMoreClick>;
     };
 
     _separator = () => {
@@ -82,32 +84,35 @@ export default class SelectAreaPage extends BasePage {
 
     _onItemClick = (item) => {
         const { setArea } = this.props.navigation.state.params || {};
-        if (this.state.tag === "province") {
+        if (this.state.tag === 'province') {
             // 跳转到市级
-            this.$navigate("mine/address/SelectAreaPage", {
+            this.$navigate('mine/address/SelectAreaPage', {
                 setArea: setArea,
-                tag: "city",
+                tag: 'city',
                 provinceCode: item.code,
                 provinceName: item.name,
                 fatherCode: item.code
             });
-        } else if (this.state.tag === "city") {
+        } else if (this.state.tag === 'city') {
             // 跳转到区级
-            this.$navigate("mine/address/SelectAreaPage", {
+            this.$navigate('mine/address/SelectAreaPage', {
                 setArea: setArea,
-                tag: "area",
+                tag: 'area',
                 provinceCode: this.props.navigation.state.params.provinceCode,
                 provinceName: this.props.navigation.state.params.provinceName,
                 cityCode: item.code,
                 cityName: item.name,
                 fatherCode: item.code
             });
-        } else if (this.state.tag === "area") {
+        } else if (this.state.tag === 'area') {
             // 回退并刷新
-            this.$navigateBack(-3);
-            const { provinceCode, provinceName, cityCode, cityName } = this.props.navigation.state.params || {};
-            let areaText = provinceName + cityName + item.name;
-            setArea && setArea(provinceCode, provinceName, cityCode, cityName, item.code, item.name, areaText);
+            if (this.canBack) {
+                this.canBack = false;
+                this.$navigateBack(-3);
+                const { provinceCode, provinceName, cityCode, cityName } = this.props.navigation.state.params || {};
+                let areaText = provinceName + cityName + item.name;
+                setArea && setArea(provinceCode, provinceName, cityCode, cityName, item.code, item.name, areaText);
+            }
         }
     };
 }
@@ -115,9 +120,9 @@ export default class SelectAreaPage extends BasePage {
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: "white",
-        flexDirection: "row",
-        alignItems: "center",
+        backgroundColor: 'white',
+        flexDirection: 'row',
+        alignItems: 'center',
         height: 48
     },
     blackText: {
