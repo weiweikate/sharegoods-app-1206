228741b3a70ed4ba89662aeff14873c31a4d95ec
max <iosyufeng@sina.com>
2019-01-10 11:55:19 +0800

    fix更改bug优化购物车

--- src/model/oldUserLoginModel.js
+++ src/model/oldUserLoginModel.js
@@ -8,6 +8,8 @@ class oldUserLoginModel {
 
     @observable
     isShowOldBtn = false;
+    @observable
+    isShowReg  = true;
 
     /**
      * @param isShowToast 是否显示toast
@@ -16,21 +18,26 @@ class oldUserLoginModel {
     checkIsShowOrNot = (isShowToast) => {
         LoginAPI.oldUserActivateJudge().then((res) => {
             console.log('是还是非-------', res);
-            this.isShowOldBtn = res.data;
+            console.log('hyf-----',res.data);
+            this.isShowOldBtn = res.data.oldSwitch;
+            this.isShowReg = res.data.regSwitch;
+
             // this.isShowOldBtn = true;
         }).catch((error) => {
             if (isShowToast) {
                 bridge.$toast(error.msg)
             }
             this.isShowOldBtn = false;
+            this.isShowReg = true;
         });
     };
     @action
     JumpToLogin = (routeName) => {
         LoginAPI.oldUserActivateJudge().then((res) => {
             console.log('是还是非-------', res);
-            this.isShowOldBtn = res.data;
-            if (res.data) {
+            this.isShowOldBtn = res.data.oldSwitch;
+            this.isShowReg = res.data.regSwitch;
+            if (res.data.oldSwitch) {
                 const navigationAction = NavigationActions.navigate({
                     routeName: routeName
                 });
@@ -49,14 +56,15 @@ class oldUserLoginModel {
     isCanTopRegist = (routeName) => {
         LoginAPI.oldUserActivateJudge().then((res) => {
             console.log('是还是非-------', res);
-            this.isShowOldBtn = res.data;
-            if (!res.data) {
+            this.isShowOldBtn = res.data.oldSwitch;
+            this.isShowReg = res.data.regSwitch;
+            if (this.isShowReg) {
                 const navigationAction = NavigationActions.navigate({
                     routeName: routeName
                 });
                 global.$navigator.dispatch(navigationAction);
             } else {
-                bridge.$toast('老用户激活阶段不可注册');
+                bridge.$toast('注册入口未开放');
             }
         }).catch((error) => {
             this.isShowOldBtn = false;
@@ -68,8 +76,9 @@ class oldUserLoginModel {
     isCanLoginWithWx = (callBack) => {
         LoginAPI.oldUserActivateJudge().then((res) => {
             console.log('是还是非-------', res);
-            this.isShowOldBtn = res.data;
-            if (!res.data) {
+            this.isShowOldBtn = res.data.oldSwitch;
+            this.isShowReg = res.data.regSwitch;
+            if (res.data.regSwitch) {
                 callBack(true);
             } else {
                 callBack(false);
