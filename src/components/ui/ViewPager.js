fe09f2debe82867bd2ae42750bc150dfe070b7bb
lymluck <894405031@qq.com>
2019-01-16 19:46:54 +0800

    fix：修复商品分类图片不展示的问题

--- src/components/ui/ViewPager.js
+++ src/components/ui/ViewPager.js
@@ -1,9 +1,9 @@
-import React ,{Component}from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import {  View ,} from 'react-native';
+import { View } from 'react-native';
 import Swiper from 'react-native-swiper';
 import EmptyUtils from '../../utils/EmptyUtils';
-import _ from 'lodash'
+import _ from 'lodash';
 // const ViewPager = props => {
 //     const {
 //         horizontal = true,
@@ -103,33 +103,34 @@ import _ from 'lodash'
 // };
 
 
-class ViewPager extends Component{
-    constructor(props){
+class ViewPager extends Component {
+    constructor(props) {
         super(props);
         this.state = {
-            arrayData:this.props.arrayData
-        }
+            arrayData: this.props.arrayData
+        };
     }
 
 
-    componentWillReceiveProps(nextprops,nextState) {
+    componentWillReceiveProps(nextprops, nextState) {
         let deff = _.differenceWith(nextprops.arrayData, this.props.arrayData, _.isEqual);
-        if(deff && deff.length > 0){
+        if (deff && deff.length > 0) {
             this.setState({
-                arrayData:[]
-            },()=>{
+                arrayData: []
+            }, () => {
                 this.setState({
-                    arrayData:nextprops.arrayData
-                })
-            })
+                    arrayData: nextprops.arrayData
+                });
+            });
         }
     }
-    shouldComponentUpdate(nextProps,nextState){
+
+    shouldComponentUpdate(nextProps, nextState) {
         let deffProps = _.differenceWith(nextProps.arrayData, this.props.arrayData, _.isEqual);
         let deffState = _.differenceWith(nextState.arrayData, this.state.arrayData, _.isEqual);
-        if((deffProps && deffProps.length > 0) || (deffState && deffState.length >  0)){
+        if ((deffProps && deffProps.length > 0) || (deffState && deffState.length > 0)) {
             return true;
-        }else {
+        } else {
             return false;
         }
     }
@@ -144,24 +145,24 @@ class ViewPager extends Component{
                         <Swiper
                             {...props}
                             style={this.props.styles ? this.props.styles.wrapper : {}}
-                                height={this.props.height}
-                                paginationStyle={{ marginBottom: -20 }}
-                                horizontal={this.props.horizontal}
-                                renderPagination={this.props.renderPagination}
-                                autoplay={this.props.autoplay}
-                                dotStyle={this.props.dotStyle}
-                                activeDotStyle={this.props.activeDotStyle}
-                                dotColor={this.props.dotColor}
-                                activeDotColor={this.props.activeDotColor}
-                                autoplayTimeout={5}
-                                removeClippedSubviews={false}
-                                showsPagination={this.props.showsPagination}
-                                onIndexChanged={this.props.onIndexChanged}
-                                index={this.props.index}
-                                containerStyle={{
-                                    height: this.props.height
-                                }}
-                               >
+                            height={this.props.height}
+                            paginationStyle={{ marginBottom: -20 }}
+                            horizontal={this.props.horizontal}
+                            renderPagination={this.props.renderPagination}
+                            autoplay={this.props.autoplay}
+                            dotStyle={this.props.dotStyle}
+                            activeDotStyle={this.props.activeDotStyle}
+                            dotColor={this.props.dotColor}
+                            activeDotColor={this.props.activeDotColor}
+                            autoplayTimeout={5}
+                            removeClippedSubviews={false}
+                            showsPagination={this.props.showsPagination}
+                            onIndexChanged={this.props.onIndexChanged}
+                            index={this.props.index}
+                            containerStyle={{
+                                height: this.props.height
+                            }}
+                        >
                             {this.renderItems()}
                         </Swiper>);
                 }
@@ -171,26 +172,26 @@ class ViewPager extends Component{
                         <Swiper
                             {...props}
                             style={this.props.styles ? this.props.styles.wrapper : {}}
-                                height={this.props.height}
-                                paginationStyle={{ marginBottom: -20 }}
-                                horizontal={this.props.horizontal}
-                                autoplay={this.props.autoplay}
-                                dotStyle={this.props.dotStyle}
-                                activeDotStyle={this.props.activeDotStyle}
-                                dotColor={this.props.dotColor}
-                                activeDotColor={this.props.activeDotColor}
-                                autoplayTimeout={5}
-                                removeClippedSubviews={false}
-                                showsPagination={this.props.showsPagination}
-                                onIndexChanged={this.props.onIndexChanged}
-                                index={this.props.index}
-                                containerStyle={{
-                                    height: this.props.height
-                                }}
-                                >
+                            height={this.props.height}
+                            paginationStyle={{ marginBottom: -20 }}
+                            horizontal={this.props.horizontal}
+                            autoplay={this.props.autoplay}
+                            dotStyle={this.props.dotStyle}
+                            activeDotStyle={this.props.activeDotStyle}
+                            dotColor={this.props.dotColor}
+                            activeDotColor={this.props.activeDotColor}
+                            autoplayTimeout={5}
+                            removeClippedSubviews={false}
+                            showsPagination={this.props.showsPagination}
+                            onIndexChanged={this.props.onIndexChanged}
+                            index={this.props.index}
+                            containerStyle={{
+                                height: this.props.height
+                            }}>
                             {this.renderItems()}
                         </Swiper>);
                 }
+                return null;
             }
         }
     };
@@ -209,8 +210,8 @@ class ViewPager extends Component{
     };
 
 
-    render(){
-        if(EmptyUtils.isEmpty(this.state.arrayData) || this.state.arrayData.length === 0){
+    render() {
+        if (EmptyUtils.isEmpty(this.state.arrayData) || this.state.arrayData.length === 0) {
             return null;
         }
 
