4613294cafb03c1ead154823171e1bc28ec6c378
lymluck <894405031@qq.com>
2019-01-21 19:31:38 +0800

    pref：优化首页样式

--- src/pages/home/HomeRecommendView.js
+++ src/pages/home/HomeRecommendView.js
@@ -1,57 +1,59 @@
 /**
  * 精品推荐
  */
-import React, {Component} from 'react'
-import { View, ScrollView, StyleSheet, TouchableOpacity } from 'react-native'
-import ScreenUtil from '../../utils/ScreenUtils'
-const { px2dp } = ScreenUtil
-import {observer} from 'mobx-react'
-import { homeModule} from './Modules'
-import DesignRule from '../../constants/DesignRule';
-import { recommendModule } from './HomeRecommendModel'
-import ImageLoad from '@mr/image-placeholder'
-import {MRText as Text} from '../../components/ui';
+import React, { Component } from 'react';
+import { View, ScrollView, StyleSheet, TouchableOpacity } from 'react-native';
+import ScreenUtil from '../../utils/ScreenUtils';
 
-const RecommendItem = ({item, press}) => <TouchableOpacity style={styles.item} onPress={()=> press && press()}>
+const { px2dp } = ScreenUtil;
+import { observer } from 'mobx-react';
+import { homeModule } from './Modules';
+import DesignRule from '../../constants/DesignRule';
+import { recommendModule } from './HomeRecommendModel';
+import ImageLoad from '@mr/image-placeholder';
+import { MRText as Text } from '../../components/ui';
+
+const RecommendItem = ({ item, press }) => <TouchableOpacity style={styles.item} onPress={() => press && press()}>
     <View style={styles.imgView}>
-        <ImageLoad cacheable={true} style={styles.img}  source={{uri:item.imgUrl}}/>
+        <ImageLoad cacheable={true} style={styles.img} source={{ uri: item.imgUrl }}/>
     </View>
-</TouchableOpacity>
+</TouchableOpacity>;
 
 @observer
 export default class HomeRecommendView extends Component {
 
     _onRecommendAction(item) {
-        let router = homeModule.homeNavigate(item.linkType, item.linkTypeCode)
-        const {navigate} = this.props
-        let params = homeModule.paramsNavigate(item)
-        navigate(router, {...params, preseat:'home_recommend'})
+        let router = homeModule.homeNavigate(item.linkType, item.linkTypeCode);
+        const { navigate } = this.props;
+        let params = homeModule.paramsNavigate(item);
+        navigate(router, { ...params, preseat: 'home_recommend' });
         // navigate && navigate(router,  params)
     }
 
     render() {
-        const { recommendList } = recommendModule
-        let items = []
+        const { recommendList } = recommendModule;
+        let items = [];
         recommendList.map((item, index) => {
-            items.push(<RecommendItem key={index} item={item} press={()=>this._onRecommendAction(item)}/>)
-        })
+            items.push(<RecommendItem key={index} item={item} press={() => this._onRecommendAction(item)}/>);
+        });
         return <View>
-        {
-            items.length > 0
-            ?
-            <View style={styles.container}>
-                <View style={styles.titleView}>
-                    <Text style={styles.title} allowFontScaling={false}>精品推荐</Text>
-                </View>
-                <ScrollView style={styles.scroll} horizontal={true} showsHorizontalScrollIndicator={false}>
-                    {items}
-                    <View style={styles.space}/>
-                </ScrollView>
-            </View>
-            :
-            null
-        }
-        </View>
+            {
+                items.length > 0
+                    ?
+                    <View style={styles.container}>
+                        <View style={styles.titleView}>
+                            <Text style={styles.title} allowFontScaling={false}>精品推荐</Text>
+                        </View>
+                        <ScrollView style={styles.scroll} horizontal={true} showsHorizontalScrollIndicator={false}>
+                            <View style={{ width: px2dp(5) }}/>
+                            {items}
+                            <View style={styles.space}/>
+                        </ScrollView>
+                    </View>
+                    :
+                    null
+            }
+        </View>;
     }
 }
 
@@ -82,7 +84,7 @@ let styles = StyleSheet.create({
         width: px2dp(280),
         height: px2dp(140),
         borderRadius: px2dp(5),
-        overflow: 'hidden',
+        overflow: 'hidden'
     },
     item: {
         width: px2dp(280),
@@ -90,11 +92,11 @@ let styles = StyleSheet.create({
         marginLeft: px2dp(10)
     },
     space: {
-        width: px2dp(10)
+        width: px2dp(15)
     },
     text: {
         color: DesignRule.textColor_secondTitle,
         fontSize: px2dp(13),
         marginTop: px2dp(10)
     }
-})
+});
