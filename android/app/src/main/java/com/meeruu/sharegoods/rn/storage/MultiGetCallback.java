01b47347b31ec476da66766c3bef33906675461c
xzm <2546308812@qq.com>
2019-01-23 14:20:52 +0800

    storage位置替换

---
+++ android/app/src/main/java/com/meeruu/sharegoods/rn/storage/MultiGetCallback.java
@@ -0,0 +1,6 @@
+package com.meeruu.sharegoods.rn.storage;
+
+public interface MultiGetCallback {
+    void onSuccess(Object data);
+    void onFail(String type);
+}
---
+++ android/app/src/main/java/com/meeruu/sharegoods/rn/storage/StorageErrorType.java
@@ -0,0 +1,7 @@
+package com.meeruu.sharegoods.rn.storage;
+
+public interface StorageErrorType {
+    String KEYERROR = "Invalid key";
+    String VALUEERROR = "Invalid Value";
+    String DBERROR = "Database Error";
+}
