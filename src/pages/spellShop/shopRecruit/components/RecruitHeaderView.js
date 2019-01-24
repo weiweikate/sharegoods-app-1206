8a3615d7fad79fd72bae137fba2041f0e2700979
slardar_chen <chen_yangjun@yeah.net>
2019-01-24 12:07:41 +0800

    perf: 招募中头像

--- src/pages/spellShop/shopRecruit/components/RecruitHeaderView.js
+++ src/pages/spellShop/shopRecruit/components/RecruitHeaderView.js
@@ -17,7 +17,8 @@ import ScreenUtils from '../../../../utils/ScreenUtils';
 import DateUtils from '../../../../utils/DateUtils';
 import StringUtils from '../../../../utils/StringUtils';
 import DesignRule from '../../../../constants/DesignRule';
-import UIImage from '@mr/image-placeholder'
+import AvatarImage from '../../../../components/ui/AvatarImage';
+
 import {
     MRText as Text
 } from '../../../../components/ui';
@@ -65,7 +66,7 @@ export default class RecommendRow extends Component {
                     flexDirection: 'row',
                     marginLeft: ScreenUtils.autoSizeWidth(57)
                 }}>
-                    <UIImage source={{ uri: headUrl }} borderRadius={30} style={styles.shopIcon}/>
+                    <AvatarImage source={{ uri: headUrl }} borderRadius={30} style={styles.shopIcon}/>
                     <View style={{ justifyContent: 'center' }}>
                         <Text style={styles.shopName} allowFontScaling={false}>{name || ''}</Text>
                         <Text style={styles.shopId} allowFontScaling={false}>ID：{showNumber || ''}</Text>
