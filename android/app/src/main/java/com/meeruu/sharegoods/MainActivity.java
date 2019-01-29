00c079ca77ab67d8d059c8a0b08dbfe0087d6dde
lymluck <894405031@qq.com>
2019-01-29 10:01:00 +0800

    pref：优化开机广告

--- android/app/src/main/java/com/meeruu/sharegoods/MainActivity.java
+++ android/app/src/main/java/com/meeruu/sharegoods/MainActivity.java
@@ -189,8 +189,7 @@ public class MainActivity extends BaseActivity {
                         mHandler.sendEmptyMessageDelayed(ParameterUtils.EMPTY_WHAT, 4000);
                         ((ViewStub) findViewById(R.id.vs_adv)).inflate();
                         ivAdv = findViewById(R.id.iv_adv);
-                        String url = ossHost + "/app/start_adv.png";
-                        ImageLoadUtils.clearCache(Uri.parse(url));
+                        String url = ossHost + "/app/start_adv.png?" + System.currentTimeMillis();
                         ImageLoadUtils.loadScaleTypeNetImage(url, ivAdv,
                                 ScalingUtils.ScaleType.FIT_CENTER, true);
                         ivAdvBg = findViewById(R.id.iv_adv_bg);
