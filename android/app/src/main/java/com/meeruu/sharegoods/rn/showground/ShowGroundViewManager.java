c17f73df73d7633002cb412058f490cb86d5abe0
xzm <2546308812@qq.com>
2019-02-18 10:14:22 +0800

    1

--- android/app/src/main/java/com/meeruu/sharegoods/rn/showground/ShowGroundViewManager.java
+++ android/app/src/main/java/com/meeruu/sharegoods/rn/showground/ShowGroundViewManager.java
@@ -1,9 +1,15 @@
 package com.meeruu.sharegoods.rn.showground;
 
+import android.content.Context;
+import android.os.Handler;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.view.LayoutInflater;
 import android.view.View;
 
 import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
+import com.meeruu.sharegoods.R;
 import com.meeruu.sharegoods.rn.waveview.WaveLoadingView;
 
 public class ShowGroundViewManager extends SimpleViewManager<View> {
@@ -16,7 +22,36 @@ public class ShowGroundViewManager extends SimpleViewManager<View> {
 
     @Override
     protected View createViewInstance(ThemedReactContext reactContext) {
-        return null;
+        LayoutInflater inflater = LayoutInflater.from(reactContext);
+        View view = inflater.inflate(R.layout.home_page, null);
+        initView(reactContext,view);
+        initData();
+        return view;
+    }
+
+    private void initView(Context context, View view) {
+        swipeRefreshLayout = view.findViewById(R.id.refresh_control);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        homePageAdapter.addData(mock.getItems());
+                        homePageAdapter.notifyDataSetChanged();
+                        swipeRefreshLayout.setRefreshing(false);
+                    }
+                }, 2000);
+            }
+        });
+        recyclerView = view.findViewById(R.id.home_recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(context));
+    }
+
+    private void initData() {
+        data.addAll(mock.getItems());
+        homePageAdapter = new HomePageAdapter(data);
+        recyclerView.setAdapter(homePageAdapter);
     }
 }
 
