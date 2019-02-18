c3eedb9d2e920b6ad3961ea1a32a881f7648ada1
xzm <2546308812@qq.com>
2019-02-18 10:12:02 +0800

    1

--- android/app/src/main/java/com/meeruu/sharegoods/rn/showground/ShowGroundViewManager.java
+++ android/app/src/main/java/com/meeruu/sharegoods/rn/showground/ShowGroundViewManager.java
@@ -1,4 +1,22 @@
 package com.meeruu.sharegoods.rn.showground;
 
-public class ShowGroundViewManager {
+import android.view.View;
+
+import com.facebook.react.uimanager.SimpleViewManager;
+import com.facebook.react.uimanager.ThemedReactContext;
+import com.meeruu.sharegoods.rn.waveview.WaveLoadingView;
+
+public class ShowGroundViewManager extends SimpleViewManager<View> {
+    private static final String COMPONENT_NAME = "ShowGroundView";
+
+    @Override
+    public String getName() {
+        return COMPONENT_NAME;
+    }
+
+    @Override
+    protected View createViewInstance(ThemedReactContext reactContext) {
+        return null;
+    }
 }
+
