702a2ad4d9855d733fdc4cd1670acb5cc8b486ec
胡超_iOS <huchao@meeruu.com>
2019-02-21 19:34:51 +0800

    1111

---
+++ ios/Class/NativeBridge/ShowGroundView/ShowHeaderView.m
@@ -0,0 +1,21 @@
+//
+//  ShowHeaderView.m
+//  crm_app_xiugou
+//
+//  Created by 胡胡超 on 2019/2/21.
+//  Copyright © 2019年 Facebook. All rights reserved.
+//
+
+#import "ShowHeaderView.h"
+
+@implementation ShowHeaderView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
---
+++ ios/Class/NativeBridge/ShowGroundView/ShowHeaderViewManager.m
@@ -0,0 +1,21 @@
+//
+//  ShowHeaderViewManager.m
+//  crm_app_xiugou
+//
+//  Created by 胡胡超 on 2019/2/21.
+//  Copyright © 2019年 Facebook. All rights reserved.
+//
+
+#import "ShowHeaderViewManager.h"
+
+@implementation ShowHeaderViewManager
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
