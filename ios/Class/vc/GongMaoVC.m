c219c8e38a08e45b6a29a2d10931b579ec0f7a95
胡超_iOS <huchao@meeruu.com>
2019-03-05 15:52:46 +0800

    ios

--- ios/Class/vc/GongMaoVC.m
+++ ios/Class/vc/GongMaoVC.m
@@ -9,7 +9,10 @@
 #import "GongMaoVC.h"
 #import <WebKit/WebKit.h>
 #import <SDAutoLayout.h>
-#import <imageco>
+#import "MBProgressHUD+PD.h"
+#import "StorageFromRN.h"
+static  NSString * constTile = @"公猫认证";
+static  NSString * web_back_mark = @"/gongmall/contract/notify";
 @interface GongMaoVC ()<WKNavigationDelegate>
 @property(nonatomic, strong)WKWebView *webView;
 @end
@@ -17,9 +20,38 @@
 @implementation GongMaoVC
 
 - (void)viewDidLoad {
-    [super viewDidLoad];
+  [super viewDidLoad];
+  self.title = constTile;
+  [self addBackBtn];
+  if (!self.url) {
+    [MBProgressHUD showError:@"网页链接为空"];
+    return;
+  }
   NSURLRequest * request = [NSURLRequest requestWithURL:[NSURL URLWithString:self.url]];
   [self.webView loadRequest:request];
+}
+/** 添加返回按钮*/
+- (void)addBackBtn
+{
+  UIButton *backBtn = [[UIButton alloc]initWithFrame:CGRectMake(0, 0, 44, 44)];
+  [backBtn addTarget:self action:@selector(back) forControlEvents:UIControlEventTouchUpInside];
+  [backBtn setImage:[UIImage imageNamed:@"back"] forState:0];
+  backBtn.imageView.sd_layout
+  .centerYEqualToView(backBtn)
+  .leftSpaceToView(backBtn, 0)
+  .heightIs(15)
+  .widthIs(15);
+  UIBarButtonItem* backItem = [[UIBarButtonItem alloc]initWithCustomView:backBtn];
+  self.navigationItem.backBarButtonItem = backItem;
+}
+/** 返回事件*/
+- (void)back
+{
+  if (self.webView.canGoBack) {
+    [self.webView goBack];
+  }else{
+    [self.navigationController popViewControllerAnimated:YES];
+  }
 }
 
 /** 懒加载*/
@@ -42,7 +74,23 @@
   NSURL * navi_url = navigationAction.request.URL;
   NSString *url_str = navi_url.absoluteString;
   NSLog(@"%@",url_str);
+  NSString *mark = [NSString stringWithFormat:@"%@%@",[StorageFromRN getHost], web_back_mark];
+  if ([mark isEqualToString:url_str]) {
+    decisionHandler(WKNavigationActionPolicyCancel);
+    [self.navigationController popViewControllerAnimated:YES];
+    return;
+  }
   decisionHandler(WKNavigationActionPolicyAllow);
 }
+
+- (void)webView:(WKWebView *)webView didFinishNavigation:(null_unspecified WKNavigation *)navigation
+{
+  if (webView.title) {
+    self.title = webView.title;
+  }else{
+    self.title = constTile;
+  }
+}
+
 
 @end
