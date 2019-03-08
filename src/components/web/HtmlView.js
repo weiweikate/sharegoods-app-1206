8313d931e414b8ccba90b3aa02c6a90cc5662036
xzm <2546308812@qq.com>
2019-03-08 14:39:45 +0800

    1

--- src/components/web/HtmlView.js
+++ src/components/web/HtmlView.js
@@ -2,10 +2,8 @@ import React from 'react';
 import BasePage from '../../BasePage';
 import WebViewBridge from '@mr/webview';
 import { View } from 'react-native';
-import ScreenUtils from '../../utils/ScreenUtils';
 import CommShareModal from '../../comm/components/CommShareModal';
 // import res from '../../comm/res';
-import ExtraDimensions from 'react-native-extra-dimensions-android';
 import apiEnvironment from '../../api/ApiEnvironment';
 
 export default class RequestDetailPage extends BasePage {
@@ -26,8 +24,8 @@ export default class RequestDetailPage extends BasePage {
         } else {
             realUri = uri + '?ts=' + new Date().getTime();
         }
-        if (realUri.indexOf('http') === -1){
-            realUri =  apiEnvironment.getCurrentH5Url() + realUri;
+        if (realUri.indexOf('http') === -1) {
+            realUri = apiEnvironment.getCurrentH5Url() + realUri;
         }
         this.state = {
             title: title,
@@ -55,55 +53,55 @@ export default class RequestDetailPage extends BasePage {
     };
 
     _render() {
-        let height = ScreenUtils.height - ScreenUtils.headerHeight;
-        if (ScreenUtils.isAllScreenDevice && !ScreenUtils.getBarShow()) {
-            height = ExtraDimensions.get('REAL_WINDOW_HEIGHT') - ScreenUtils.headerHeight;
-        } else if (ScreenUtils.isAllScreenDevice && ScreenUtils.getBarShow()) {
-            height = ScreenUtils.height - 44- ExtraDimensions.get('STATUS_BAR_HEIGHT');
-        }
+        // let height = ScreenUtils.height - ScreenUtils.headerHeight;
+        // if (ScreenUtils.isAllScreenDevice && !ScreenUtils.getBarShow()) {
+        //     height = ExtraDimensions.get('REAL_WINDOW_HEIGHT') - ScreenUtils.headerHeight;
+        // } else if (ScreenUtils.isAllScreenDevice && ScreenUtils.getBarShow()) {
+        //     height = ScreenUtils.height - 44- ExtraDimensions.get('STATUS_BAR_HEIGHT');
+        // }
         return (
-            <View style={{ height, overflow: 'hidden' }}>
-                <WebViewBridge
-                    style={{ flex: 1 }}
-                    ref={(ref) => {
-                        this.webView = ref;
-                    }}
-                    originWhitelist={['(.*?)']}
-                    source={{ uri: this.state.uri }}
-                    navigateAppPage={(r, p) => {
-                        if (r === 'login/login/LoginPage') {
-                            this.$navigate(r, {
-                                ...p, callback: () => {
-                                    this.webView && this.webView.reload();
-                                }
-                            });
-                        } else {
-                            this.$navigate(r, p);
-                        }
-                    }}
-                    onNavigationStateChange={event => {
-                        this.canGoBack = event.canGoBack;
-                        this.$NavigationBarResetTitle(this.state.title || event.title);
-                    }}
-                    onError={event => {
-                        this.canGoBack = event.canGoBack;
-                        this.$NavigationBarResetTitle('加载失败');
-                    }}
+                <View style={{ flex: 1, overflow: 'hidden' }}>
+                    <WebViewBridge
+                        style={{ flex: 1 }}
+                        ref={(ref) => {
+                            this.webView = ref;
+                        }}
+                        originWhitelist={['(.*?)']}
+                        source={{ uri: this.state.uri }}
+                        navigateAppPage={(r, p) => {
+                            if (r === 'login/login/LoginPage') {
+                                this.$navigate(r, {
+                                    ...p, callback: () => {
+                                        this.webView && this.webView.reload();
+                                    }
+                                });
+                            } else {
+                                this.$navigate(r, p);
+                            }
+                        }}
+                        onNavigationStateChange={event => {
+                            this.canGoBack = event.canGoBack;
+                            this.$NavigationBarResetTitle(this.state.title || event.title);
+                        }}
+                        onError={event => {
+                            this.canGoBack = event.canGoBack;
+                            this.$NavigationBarResetTitle('加载失败');
+                        }}
 
-                    // onLoadStart={() => this._onLoadStart()}
-                    onLoadEnd={(event) => {
-                        this.canGoBack = event.canGoBack;
-                    }}
-                    postMessage={msg => this._postMessage(msg)}
-                />
-                <CommShareModal
-                    ref={(ref) => this.shareModal = ref}
-                    reloadWeb={() => {
-                        this.webView && this.webView.reload();
-                    }}
-                    {...this.state.shareParmas}
-                />
-            </View>
+                        // onLoadStart={() => this._onLoadStart()}
+                        onLoadEnd={(event) => {
+                            this.canGoBack = event.canGoBack;
+                        }}
+                        postMessage={msg => this._postMessage(msg)}
+                    />
+                    <CommShareModal
+                        ref={(ref) => this.shareModal = ref}
+                        reloadWeb={() => {
+                            this.webView && this.webView.reload();
+                        }}
+                        {...this.state.shareParmas}
+                    />
+                </View>
         );
     }
 }
