7a03ac3fef189f293a1a4a97a421f48995b9b705
huyufeng <iosyufeng@sina.com>
2019-03-19 14:54:35 +0800

    fix：客服增加桥接类

---
+++ ios/Class/Modules/CustomService-客服/JRServiceBridge.m
@@ -0,0 +1,16 @@
+//
+//  JRServiceBridge.m
+//  crm_app_xiugou
+//
+//  Created by 胡玉峰 on 2019/3/19.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+#import "JRServiceBridge.h"
+#import <React/RCTBridge.h>
+
+@implementation JRServiceBridge
+
+
+
+@end
