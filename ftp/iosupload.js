351a8b29ff1f8612786b0ad763c4c1d601534819
huyufeng <iosyufeng@sina.com>
2019-03-20 17:25:49 +0800

    fix:自动化上传

--- ftp/iosupload.js
+++ ftp/iosupload.js
@@ -8,58 +8,93 @@ var fs = require("fs");
 
 var c = new Client();
 var config = {
-    host:'172.16.10.253',
-    port:21,
-    user:'yufeng.h',
-    password:'Hyf*123456'
+    host: '172.16.10.253',
+    port: 21,
+    user: 'yufeng.h',
+    password: 'Hyf*123456'
 }
-var remoatPath = '/package/test/ios';
 var currDate = new Date();
-var dirName = `/${currDate.getFullYear()}.${currDate.getMonth()+1}.${currDate.getDate()}-${currDate.getHours()}.${currDate.getMinutes()}.${currDate.getMinutes()}`;
-var dir = remoatPath+dirName;
-var localfile = '/Users/huyufeng/Documents/appStore/adhoc/1.1.0.1/222.ipa';
+var dirName = `/feng${currDate.getFullYear()}.${currDate.getMonth() + 1}.${currDate.getDate()}-${currDate.getHours()}.${currDate.getMinutes()}.${currDate.getMinutes()}`;
+var localfile = '/Users/mac/Desktop/ipa/crm_app_xiugou.ipa';
+
+function getEnviroment(callBack) {
+    fs.readFile('./config.json', function(err, data) {
+        if (err) {
+            return console.error(err);
+        }
+        var config = JSON.parse(data);//将字符串转换为json对象
+        callBack && callBack(config.envType,config.Version)
+    })
+}
+
+
+
+
+function getRemoatPath(callBack) {
+    getEnviroment((evnTyp,dirName) => {
+        console.log(config.envType)
+        var endDir = '';
+        if (evnTyp === 'qa') {
+            endDir = '/package/test/ios/'+dirName;
+        } else if (evnTyp === 'pre_release') {
+            endDir = '/package/uat/ios/' + dirName;
+        } else if (evnTyp === 'garyTest') {
+            endDir = '/package/gray/ios/' + dirName;
+        } else if (evnTyp === 'online') {
+            endDir = '/package/online/ios/' + dirName;
+        }
+        callBack && callBack(endDir)
+    })
+}
 
 function cdRemoteDir() {
-    c.cwd(dir,function(error,currDir) {
-        if(error){
-            console.error(error);
-            return;
-        }
-        upload();
+    getRemoatPath((endDir)=>{
+        c.cwd(endDir, function(error, currDir) {
+            if (error) {
+                console.error(error);
+                return;
+            }
+            upload();
+        })
     })
 }
 
 function upload() {
     let remoteFile = 'release.ipa';
-    c.put(localfile,remoteFile,function(error) {
-        if(error){
+    c.put(localfile, remoteFile, function(error) {
+        if (error) {
             console.error(error);
             throw error;
             c.end();
-        }else {
+        } else {
             console.log('上传成功');
             c.end();
         }
     })
 }
 
-function makeDir (){
-    console.log(dir);
+function makeDir() {
     console.log(localfile);
-    c.mkdir(dir,function() {
-        fs.exists(localfile,function(exists) {
-            if(!exists){
-                throw new Error('本地ipa文件不存在');
-                c.end();
-            }else {
-                console.log('上传中');
-                cdRemoteDir();
-            }
+    getRemoatPath((endDir)=>{
+        console.log('远程地址'+endDir);
+        console.log('本地地址'+localfile)
+        console.log(__dirname)
+        c.mkdir(endDir, function() {
+            fs.exists(localfile, function(exists) {
+                if (!exists) {
+                    throw new Error('本地ipa文件不存在');
+                    c.end();
+                } else {
+                    console.log('上传中');
+                    cdRemoteDir();
+                }
+            })
         })
     })
+
 }
 
-c.on('ready',function() {
+c.on('ready', function() {
     makeDir();
 });
 c.connect(config)
