66d7a4adf41bca478f361f0b4bfa250e05d9f418
huyufeng <iosyufeng@sina.com>
2019-03-20 15:01:23 +0800

    fix

--- src/utils/TrackEvent/Message.js
+++ src/utils/TrackEvent/Message.js
@@ -1,39 +1,39 @@
 //     消息模块
 const MessageModular = {
-    "ViewMyMessage": {
-        "des": '查看-信息通知',
-        "name": "ViewMyMessage",
-        "params": {}
+    "ViewMyMessage":{
+        "des":'查看-信息通知',
+        "name":"ViewMyMessage",
+        "params":{}
     },
-    "ViewNotice": {
-        "des": '查看通知',
-        "name": "ViewNotice",
-        "params": {}
+    "ViewNotice":{
+        "des":'查看通知',
+        "name":"ViewNotice",
+        "params":{}
     },
-    "ViewNoticeContent": {
-        "des": '查看通知详细信息',
-        "name": "ViewNoticeContent",
-        "params": {}
+    "ViewNoticeContent":{
+        "des":'查看通知详细信息',
+        "name":"ViewNoticeContent",
+        "params":{}
     },
-    "ViewMessage": {
-        "des": '查看消息',
-        "name": "ViewMessage",
-        "params": {}
+    "ViewMessage":{
+        "des":'查看消息',
+        "name":"ViewMessage",
+        "params":{}
     },
-    "ViewMessageContent": {
-        "des": '查看消息内详细信息',
-        "name": "ViewMessageContent",
-        "params": {}
+    "ViewMessageContent":{
+        "des":'查看消息内详细信息',
+        "name":"ViewMessageContent",
+        "params":{}
     },
-    "ViewPinMessage": {
-        "des": '查看拼店消息',
-        "name": "ViewMessage",
-        "params": {}
+    "ViewPinMessage":{
+        "des":'查看拼店消息',
+        "name":"ViewMessage",
+        "params":{}
     },
-    "ViewPinMessageContent": {
-        "des": '查看拼店内详细信息',
-        "name": "ViewPinMessageContent",
-        "params": {}
+    "ViewPinMessageContent":{
+        "des":'查看拼店内详细信息',
+        "name":"ViewPinMessageContent",
+        "params":{}
     }
-};
+}
 export default MessageModular
--- src/utils/TrackEvent/Mine.js
+++ src/utils/TrackEvent/Mine.js
@@ -1,108 +1,74 @@
 const MineModular = {
-    //点击修改头像
-    "ClickModifyAvatar": {
-        "name": "ClickModifyAvatar"
-    },
-    //修改头像成功
-    "ModifuAvatarSuccess_camera": {
-        "name": "ModifuAvatarSuccess",
-        "params": {
-            "modificationMode": 1//拍照
-        }
-    },
 
-    "ModifuAvatarSuccess_photo": {
-        "name": "ModifuAvatarSuccess",
-        "params": {
-            "modificationMode": 2//相册
-        }
-
+    "ViewPersonalInfo":{
+        "des":'查看个人资料',
+        "name":"ViewPersonalInfo",
+        "params":{}
     },
-    //点击修改昵称
-    "ClickModifyNickName": {
-        "name": "ClickModifyNickName"
+    "ViewLevelInterest":{
+        "des":'查看等级权益',
+        "name":"ViewLevelInterest",
+        "params":{}
     },
-    //修改昵称成功
-    "ModifyNickNameSuccess": {
-        "name": "ModifyNickNameSuccess"
+    "ViewHowTo":{
+        "des":'点击查看“如何玩转秀购”',
+        "name":"ViewHowTo",
+        "params":{}
     },
-    //点击实名认证
-    "ClickRealCodeentityVerify": {
-        "name": "ClickRealCodeentityVerify"
+    "ViewAccountBalance":{
+        "des":'点击查看账户余额',
+        "name":"ViewAccountBalance",
+        "params":{}
     },
-    //实名认证成功
-    "ReadCodeentityVerifySuccss": {
-        "name": "ReadCodeentityVerifySuccss"
+    "ViewShowDou":{
+        "des":'点击查看秀豆',
+        "name":"ViewShowDou",
+        "params":{}
     },
-    "ViewPersonalInfo": {
-        "des": '查看个人资料',
-        "name": "ViewPersonalInfo",
-        "params": {}
+    "ViewWaitToRecord":{
+        "des":'点击查看待入帐',
+        "name":"ViewWaitToRecord",
+        "params":{}
     },
-    "ViewLevelInterest": {
-        "des": '查看等级权益',
-        "name": "ViewLevelInterest",
-        "params": {}
+    "ViewCoupon":{
+        "des":'点击查看优惠券',
+        "name":"ViewCoupon",
+        "params":{}
     },
-    "ViewHowTo": {
-        "des": '点击查看“如何玩转秀购”',
-        "name": "ViewHowTo",
-        "params": {}
+    "ViewMyInfos":{
+        "des":'点击查看我的资料',
+        "name":"ViewMyInfos",
+        "params":{}
     },
-    "ViewAccountBalance": {
-        "des": '点击查看账户余额',
-        "name": "ViewAccountBalance",
-        "params": {}
+    "ViewMyPinCollection":{
+        "des":'点击查看收藏店铺',
+        "name":"ViewMyPinCollection",
+        "params":{}
     },
-    "ViewShowDou": {
-        "des": '点击查看秀豆',
-        "name": "ViewShowDou",
-        "params": {}
+    "ViewMyXiuCollection":{
+        "des":'点击查看秀场收藏',
+        "name":"ViewMyXiuCollection",
+        "params":{}
     },
-    "ViewWaitToRecord": {
-        "des": '点击查看待入帐',
-        "name": "ViewWaitToRecord",
-        "params": {}
+    "ViewMyFans":{
+        "des":'点击查看我的秀迷',
+        "name":"ViewMyFans",
+        "params":{}
     },
-    "ViewCoupon": {
-        "des": '点击查看优惠券',
-        "name": "ViewCoupon",
-        "params": {}
+    "ViewMyAdviser":{
+        "des":'点击查看服务顾问',
+        "name":"ViewMyAdviser",
+        "params":{}
     },
-    "ViewMyInfos": {
-        "des": '点击查看我的资料',
-        "name": "ViewMyInfos",
-        "params": {}
+    "ClickCustomerService":{
+        "des":'点击帮助与客服',
+        "name":"ClickCustomerService",
+        "params":{}
     },
-    "ViewMyPinCollection": {
-        "des": '点击查看收藏店铺',
-        "name": "ViewMyPinCollection",
-        "params": {}
-    },
-    "ViewMyXiuCollection": {
-        "des": '点击查看秀场收藏',
-        "name": "ViewMyXiuCollection",
-        "params": {}
-    },
-    "ViewMyFans": {
-        "des": '点击查看我的秀迷',
-        "name": "ViewMyFans",
-        "params": {}
-    },
-    "ViewMyAdviser": {
-        "des": '点击查看服务顾问',
-        "name": "ViewMyAdviser",
-        "params": {}
-    },
-    "ClickCustomerService": {
-        "des": '点击帮助与客服',
-        "name": "ClickCustomerService",
-        "params": {}
-    },
-    "ClickContactCustomerService": {
-        "des": '点击联系客服',
-        "name": "ClickContactCustomerService",
-        "params": {}
+    "ClickContactCustomerService":{
+        "des":'点击联系客服',
+        "name":"ClickContactCustomerService",
+        "params":{}
     }
 };
 
