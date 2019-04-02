5513691f7d550522e256f9924460bcfe10b0d833
胡超_iOS <huchao@meeruu.com>
2019-04-02 14:40:00 +0800

    1.设置登录密码UI基本逻辑完成。 2.登录密码是否设置、发送验证码、验证码验证、设置登录密码，接口未对接

--- src/pages/mine/page/myaccount/SetPhonePwdPage.js
+++ src/pages/mine/page/myaccount/SetPhonePwdPage.js
@@ -9,12 +9,10 @@ import ScreenUtils from '../../../../utils/ScreenUtils';
 import MineAPI from '../../api/MineApi';
 import bridge from '../../../../utils/bridge';
 import StringUtils from '../../../../utils/StringUtils';
-import user from '../../../../model/user';
-import shopCartStore from '../../../shopCart/model/ShopCartStore';
 import DesignRule from '../../../../constants/DesignRule';
 import { MRText as Text} from '../../../../components/ui';
 import PasswordInputText from './PasswordInputText';
-const title = '修改登录密码';
+const title = '请设置登录密码';
 const tip = '请设置6-8位数字字母组合密码，不含特殊符号';
 
 export default class EditPhonePwdPage extends BasePage {
@@ -26,7 +24,6 @@ export default class EditPhonePwdPage extends BasePage {
     constructor(props) {
         super(props);
         this.state = {
-            oldPwd: '',
             newPwd: '',
             newPwdAgain: ''
         };
@@ -34,13 +31,12 @@ export default class EditPhonePwdPage extends BasePage {
     }
 
     _render() {
-        let {oldPwd, newPwd, newPwdAgain} = this.state;
-        let enabled = oldPwd.length > 0 && newPwd.length>0 && newPwdAgain.length>0 ;
+        let {newPwd, newPwdAgain} = this.state;
+        let enabled = newPwd.length>0 && newPwdAgain.length>0 ;
         return(
             <KeyboardAvoidingView style={{alignItems: 'center'}}>
                 <Text style={styles.title}>{title}</Text>
                 <Text style={styles.tip}>{tip}</Text>
-                <PasswordInputText placeholder={'请输入原密码'} onChangeText={(text)=>{this.setState({oldPwd: text})}}/>
                 <PasswordInputText placeholder={'请输入新密码'} onChangeText={(text)=>{this.setState({newPwd: text})}}/>
                 <PasswordInputText placeholder={'请再次输入密码'} onChangeText={(text)=>{this.setState({newPwdAgain: text})}}/>
                 <TouchableOpacity style={[{
@@ -57,54 +53,29 @@ export default class EditPhonePwdPage extends BasePage {
         if (this.isLoadding === true) {
             return;
         }
-        // 密码修改成功，请重新登录
-        if (StringUtils.isEmpty(this.state.oldPwd)) {
-            bridge.$toast('旧密码不能为空');
+       let {newPwd, newPwdAgain} = this.state;
+        if (StringUtils.isEmpty(newPwd)) {
+            bridge.$toast('密码不能为空');
             return;
         }
-        if (StringUtils.isEmpty(this.state.newPwd)) {
-            bridge.$toast('新密码不能为空');
+        if (StringUtils.isEmpty(newPwdAgain)) {
+            bridge.$toast('请再次输入密码');
             return;
         }
-        if (StringUtils.isEmpty(this.state.newPwdAgain)) {
-            bridge.$toast('请再次输入新密码');
+        if (newPwdAgain !== newPwd) {
+            bridge.$toast('请确保两次输入的密码一致');
             return;
         }
-        if (this.state.newPwdAgain !== this.state.newPwd) {
-            bridge.$toast('请确保两次输入的新密码一致');
-            return;
-        }
-        if (!StringUtils.checkPassword(this.state.newPwdAgain)) {
+        if (!StringUtils.checkPassword(newPwdAgain)) {
             bridge.$toast('新密码需数字、字母组合');
             return;
         }
         this.isLoadding == true;
         MineAPI.changePhonePwd({
-            oldPassword: this.state.oldPwd,
-            newPassword: this.state.newPwd
+            newPassword: newPwd
         }).then((data) => {
-            // 退出登录
-            MineAPI.signOut().then((response) => {
-                if (response.code === 10000) {
-                    // 正常退出，或者登录超时，都去清空数据
-                    user.clearUserInfo();
-                    //清空购物车
-                    shopCartStore.data = [];
-                    this.$navigateResetLogin();
-                    bridge.$toast('密码修改成功，请重新登录');
-                }
-                this.isLoadding == false;
-            }).catch(err => {
-                bridge.$toast(err.msg);
-                this.isLoadding == false;
-                if (err.code === 10009) {
-                    user.clearUserInfo();
-                    shopCartStore.data = [];
-                    this.$navigateResetLogin();
-                    // this.$navigateReset();
-                    // this.$navigate('login/login/LoginPage');
-                }
-            });
+            this.$navigateBack(-2);
+            this.isLoadding == false;
         }).catch((data) => {
             this.isLoadding == false;
             bridge.$toast(data.msg);
