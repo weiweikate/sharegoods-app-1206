37107535641ad2b420d33b46138311f050002760
xiangchen <826464423@qq.com>
2019-04-09 13:49:17 +0800

    fix一元券点击

--- src/pages/order/model/ConfirmOrderModel.js
+++ src/pages/order/model/ConfirmOrderModel.js
@@ -96,7 +96,7 @@ class ConfirmOrderModel {
             case 98:
                 OrderApi.makeSureOrder({
                     orderType: 2,//1.普通订单 2.活动订单  -- 下单必传
-                    orderSubType: 5,//1.秒杀 2.降价拍 3.升级礼包 4.普通礼包
+                    orderSubType: 5,//1.秒杀 2.降价拍 3.升级礼包 4.普通礼包，
                     source: orderParamVO.source,//1.购物车 2.直接下单
                     channel: 2,//1.小程序 2.APP 3.H5
                     orderProductList: orderParamVO.orderProducts,
