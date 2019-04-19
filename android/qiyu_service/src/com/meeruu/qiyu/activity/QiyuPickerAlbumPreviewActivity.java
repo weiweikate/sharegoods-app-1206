1eb626f1ce3671d0af0e4fe071ae7237d25af4f2
lymluck <894405031@qq.com>
2019-04-19 20:18:26 +0800

    fix：优化Android沉浸式状态栏

--- android/qiyu_service/src/com/meeruu/qiyu/activity/QiyuPickerAlbumPreviewActivity.java
+++ android/qiyu_service/src/com/meeruu/qiyu/activity/QiyuPickerAlbumPreviewActivity.java
@@ -5,7 +5,7 @@ import android.view.View;
 import android.widget.LinearLayout;
 
 import com.meeruu.qiyu.ScreenUtils;
-import com.meeruu.qiyu.StatusBarUtils;
+import com.meeruu.statusbar.ImmersionBar;
 import com.qiyukf.nim.uikit.common.media.picker.activity.PickerAlbumPreviewActivity;
 import com.qiyukf.unicorn.R;
 
@@ -14,44 +14,10 @@ public class QiyuPickerAlbumPreviewActivity extends PickerAlbumPreviewActivity {
     @Override
     public void onCreate(Bundle bundle) {
         super.onCreate(bundle);
-        int result = StatusBarUtils.setLightMode(this);
-        int statusColor = android.R.color.white;
-        if (result == 3) {
-            // 6.0以上沉浸式
-            StatusBarUtils.setColor(this, getResources().getColor(statusColor), 0);
-            handleTop();
-        } else if (result == 4) {
-            // 其它半透明效果
-            StatusBarUtils.setColor(this, getResources().getColor(statusColor));
-        } else {
-            // miui、flyme沉浸式
-            StatusBarUtils.setColor(this, getResources().getColor(statusColor), 0);
-            handleTop();
-        }
-    }
-
-    private void handleTop() {
-        View titleBar = findViewById(R.id.ysf_title_bar);
-        titleBar.setPadding(0, ScreenUtils.getStatusHeight(this), 0, 0);
-        LinearLayout.LayoutParams param = (LinearLayout.LayoutParams) titleBar.getLayoutParams();
-        param.height += ScreenUtils.getStatusHeight(this);
-        titleBar.setLayoutParams(param);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        int result = StatusBarUtils.setLightMode(this);
-        int statusColor = android.R.color.white;
-        if (result == 3) {
-            // 6.0以上沉浸式
-            StatusBarUtils.setColor(this, getResources().getColor(statusColor), 0);
-        } else if (result == 4) {
-            // 其它半透明效果
-            StatusBarUtils.setColor(this, getResources().getColor(statusColor));
-        } else {
-            // miui、flyme沉浸式
-            StatusBarUtils.setColor(this, getResources().getColor(statusColor), 0);
-        }
+        ImmersionBar.with(this).barColor(R.color.ysf_black_b3000000)
+                .fitsSystemWindows(true)
+                .navigationBarColor(android.R.color.white)
+                .statusBarDarkFont(true)
+                .navigationBarDarkIcon(true).init();
     }
 }
--- android/qiyu_service/src/com/meeruu/qiyu/activity/QiyuWatchVideoActivity.java
+++ android/qiyu_service/src/com/meeruu/qiyu/activity/QiyuWatchVideoActivity.java
@@ -2,7 +2,7 @@ package com.meeruu.qiyu.activity;
 
 import android.os.Bundle;
 
-import com.meeruu.qiyu.StatusBarUtils;
+import com.meeruu.statusbar.ImmersionBar;
 import com.qiyukf.nim.uikit.session.activity.WatchVideoActivity;
 
 public class QiyuWatchVideoActivity extends WatchVideoActivity {
@@ -11,34 +11,10 @@ public class QiyuWatchVideoActivity extends WatchVideoActivity {
     @Override
     public void onCreate(Bundle bundle) {
         super.onCreate(bundle);
-        int result = StatusBarUtils.setLightMode(this);
-        int statusColor = android.R.color.white;
-        if (result == 3) {
-            // 6.0以上沉浸式
-            StatusBarUtils.setColor(this, getResources().getColor(statusColor), 0);
-        } else if (result == 4) {
-            // 其它半透明效果
-            StatusBarUtils.setColor(this, getResources().getColor(statusColor));
-        } else {
-            // miui、flyme沉浸式
-            StatusBarUtils.setColor(this, getResources().getColor(statusColor), 0);
-        }
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        int result = StatusBarUtils.setLightMode(this);
-        int statusColor = android.R.color.white;
-        if (result == 3) {
-            // 6.0以上沉浸式
-            StatusBarUtils.setColor(this, getResources().getColor(statusColor), 0);
-        } else if (result == 4) {
-            // 其它半透明效果
-            StatusBarUtils.setColor(this, getResources().getColor(statusColor));
-        } else {
-            // miui、flyme沉浸式
-            StatusBarUtils.setColor(this, getResources().getColor(statusColor), 0);
-        }
+        ImmersionBar.with(this)
+                .transparentBar()
+                .navigationBarColor(android.R.color.white)
+                .statusBarDarkFont(true)
+                .navigationBarDarkIcon(true).init();
     }
 }
