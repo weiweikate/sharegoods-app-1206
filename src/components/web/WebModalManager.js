e4e8ea3adb53448ce04d0185ee398fa39e6d9288
胡超_iOS <huchao@meeruu.com>
2019-04-22 18:02:42 +0800

    添加注释

--- src/components/web/WebModalManager.js
+++ src/components/web/WebModalManager.js
@@ -16,20 +16,19 @@ class Manager {
     needShowAd = false;
     @observable
     AdData = null;
-    //一天弹一次 公告与广告不共存
+    //showPage：
     // EXCHANGE(2, "兑换专区"),web
-    // EXPERIENCE(3, "经验值专区"),
-    // TOPIC(4, "专题"),
-    // CUSTOMTOPIC(5, "自定义专题"）
+    // EXPERIENCE(3, "经验值专区"),web
+    // TOPIC(4, "专题"),app
+    // CUSTOMTOPIC(5, "自定义专题"）web
     @action
-    getAd(showPage,showPageValue) {
+    getAd(showPage,showPageValue) { //获取数据
         let currStr = new Date().getTime() + "";
         let _showPageValue = showPageValue|| ""
         let _showPage = showPage|| ""
+        //将showPage，showPageValue作为本地缓存的key，取上次打开时间，如果小于一天就请求接口
         this.type = "web_storage_"+_showPage+'_'+_showPageValue;
-
         AsyncStorage.getItem(this.type).then((value) => {
-
             if (value == null || parseInt(currStr) - parseInt(value) > 24 * 60 * 60 * 1000) {
                 HomeAPI.getHomeData({showPage, showPageValue}).then(resp => {
                     if (resp.data && resp.data.length > 0) {
@@ -47,7 +46,7 @@ class Manager {
         });
     }
     @action
-    showAd(callBack){
+    showAd(callBack){//展示广告
         if (this.needShowAd === true){
             this.isShowAd= true
         }else {
@@ -57,7 +56,7 @@ class Manager {
     }
 
     @action
-    closeAd () {
+    closeAd () {//关闭广告，将关闭广告的时间保存下来
         let currStr = new Date().getTime() + '';
         AsyncStorage.setItem(this.type, currStr);
         this.isShowAd = false;
