490de1794fce9a7d7038dcef110db63bd9ed0005
ziming.x <xieziming@meeruu.com>
2019-04-22 15:56:41 +0800

    auth sdk升级

--- android/app/src/main/java/com/meeruu/sharegoods/rn/module/PhoneAuthenModule.java
+++ android/app/src/main/java/com/meeruu/sharegoods/rn/module/PhoneAuthenModule.java
@@ -1,27 +1,35 @@
 package com.meeruu.sharegoods.rn.module;
 
+import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Build;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.util.SparseArray;
+import android.widget.Toast;
 
-import com.alicom.phonenumberauthsdk.gatewayauth.AlicomAuthHelper;
-import com.alicom.phonenumberauthsdk.gatewayauth.TokenResultListener;
-import com.alicom.phonenumberauthsdk.gatewayauth.model.InitResult;
+//import com.alicom.phonenumberauthsdk.gatewayauth.AlicomAuthHelper;
+//import com.alicom.phonenumberauthsdk.gatewayauth.TokenResultListener;
+//import com.alicom.phonenumberauthsdk.gatewayauth.model.InitResult;
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.WritableMap;
+import com.mobile.auth.gatewayauth.PhoneNumberAuthHelper;
+import com.mobile.auth.gatewayauth.TokenResultListener;
+import com.mobile.auth.gatewayauth.model.InitResult;
 
 public class PhoneAuthenModule extends ReactContextBaseJavaModule {
 
     private ReactApplicationContext mContext;
     public static final String MODULE_NAME = "PhoneAuthenModule";
-    private AlicomAuthHelper mAlicomAuthHelper;
+//    private AlicomAuthHelper mAlicomAuthHelper;
     private InitResult mAutInitResult;
     private Promise authPromise;
+    private TokenResultListener mTokenListener;
+    private PhoneNumberAuthHelper mAlicomAuthHelper;
 
     private int mCurrentPermissionRequestCode = 0;
     private SparseArray<PermissionCallback> mPerMissionCallbackCache;
@@ -34,30 +42,70 @@ public class PhoneAuthenModule extends ReactContextBaseJavaModule {
     public PhoneAuthenModule(ReactApplicationContext reactContext) {
         super(reactContext);
         this.mContext = reactContext;
-        mAlicomAuthHelper = AlicomAuthHelper.getInstance(reactContext, new TokenResultListener() {
+//        mAlicomAuthHelper = AlicomAuthHelper.getInstance(reactContext, new TokenResultListener() {
+//            @Override
+//            public void onTokenSuccess(String s) {
+//                if (authPromise != null) {
+//                    WritableMap map = Arguments.createMap();
+//                    map.putInt("isCanAuthen", 1);
+//                    map.putInt("resultCode", 6666);
+//                    map.putString("data", s);
+//                    map.putString("phoneNum", mAutInitResult.getSimPhoneNumber());
+//                    authPromise.resolve(map);
+//                }
+//            }
+//
+//            @Override
+//            public void onTokenFailed(String s) {
+//                if (authPromise != null) {
+//                    WritableMap map = Arguments.createMap();
+//                    map.putInt("isCanAuthen", -1);
+//                    map.putString("phoneNum", mAutInitResult.getSimPhoneNumber());
+//                    authPromise.resolve(map);
+//                }
+//            }
+//        });
+//        mAutInitResult = mAlicomAuthHelper.init();
+        init();
+
+
+    }
+
+    private void init(){
+        mTokenListener = new TokenResultListener() {
             @Override
             public void onTokenSuccess(String s) {
-                if (authPromise != null) {
-                    WritableMap map = Arguments.createMap();
-                    map.putInt("isCanAuthen", 1);
-                    map.putInt("resultCode", 6666);
-                    map.putString("data", s);
-                    map.putString("phoneNum", mAutInitResult.getSimPhoneNumber());
-                    authPromise.resolve(map);
-                }
+
             }
 
             @Override
             public void onTokenFailed(String s) {
-                if (authPromise != null) {
-                    WritableMap map = Arguments.createMap();
-                    map.putInt("isCanAuthen", -1);
-                    map.putString("phoneNum", mAutInitResult.getSimPhoneNumber());
-                    authPromise.resolve(map);
-                }
+
             }
-        });
-        mAutInitResult = mAlicomAuthHelper.init();
+        };
+        mAlicomAuthHelper = PhoneNumberAuthHelper.getInstance(this.mContext,mTokenListener);
+        mAlicomAuthHelper.setDebugMode(false);
+        if (ActivityCompat.checkSelfPermission(this.mContext, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            requestPermission(new String[]{Manifest.permission.READ_PHONE_STATE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, new PermissionCallback() {
+                @Override
+                public void onPermissionGranted(boolean isRequestUser) {
+                    /*
+                     *   5.sdk init
+                     */
+                    mAutInitResult = mAlicomAuthHelper.init();
+                }
+
+                @Override
+                public void onPermissionDenied(boolean isRequestUser) {
+//                    Toast.makeText(MainActivity.this, "请允许相关权限", Toast.LENGTH_LONG).show();
+                }
+            });
+        } else {
+            /*
+             *   5.sdk init
+             */
+            mAutInitResult = mAlicomAuthHelper.init();
+        }
     }
 
     /**
@@ -87,7 +135,7 @@ public class PhoneAuthenModule extends ReactContextBaseJavaModule {
         // accessCode = "eyAgImsiIDogIk0xOTJYT3lYR0Y1QWdRTWhuMitucENrYmxjZDFPemxRemxzUWdESTRjYjlOWE5Pc1k1NVNRcHpJTmFLZ2F0eCtTTkYydGx0NWVqNE5cL1wvNGhDWDFWcWg3ZnNhUGQrRG9JMDlBN2htdmR4MmVONVJWaE4wb0F6K3VpcFRqVW9TVWVjRFlFTzVXbzhNOXE0eWFUWHZJUktEbUt4VERqUzcwalNBWDhpOHN3NUZETXVxWFlWSFN6MVZ3NW9jdlZKMkJzV1VZMkN1SHZDNTZEazRIRXpMZkpzcFh2d0E4TTZKNEpETUR4THh3dWprVzZBbkw5SjJRMTJEUDFuVmZcL2VzYkJ6MGhpOXp3Q1BUMFAxQjkyRmJlTFEreElyZmdUNkJvZ1FDeVpTbFpzZmJHb2VLU3Q0d2pKOEFwR3FpVncwc3kwQllEM1NiRVVVMUt6ckQ3eDhzOWJ5dz09IiwgICJ1IiA6ICIiLCAgImMiIDogIldiYmlHaUErcU5CeFFVbHVvdDczUTlUN0FwR1FBODZ1OWQySUhWb2JPbkx6d290eUVBUVlUaVExNUljS3FDUHhGaEFXRzJaR0h0T0ZiS1wvVktoalF3ZjU0OUZRdXAzQWNZczg3Z3h6cVlBdUJFc1NGeGYzNm1CdzhGeVhMT1lVU0MrR3Z5b3pZeGQ2U3ZJQWdzTEpHWVdFdGkydVdHaitXUzIxVUVWeVh1R1dsQ1R3QTc5Mk1Sdnp3RHdGV1lIaG00RzB5Q0d1UkZ5b0dJNEFzejcwdDljM0NKUENoUkxnWEw2WXV4YWN6UjhtZmxJQ00yaUVRTmh1U1JDbllOUDJZYm1TZFlLRXdncW5ZOGIzZWU4akJTS2J6VGh1VGF1b3pjbURncHZlS0JBXC9DZ3I3a05BZFdsSzFQK2p0Yk1mVHlGRk9sdHRKUVEwd3NqdHh6VjJrbHFzc3paUXh1S2N0NlwvM0M3cUFrdlppRE5GaFJOSmtBYzh3SVVTaWZ6amQ5eGNGVElLQVYrSityMHZMZmZvQlI0SDc2MVRpM285TG03eFdNOUd3d3c4eldzV1FqVlVsMVNYVGVcL1hJSllkUXlWelhxa2RQeVBuZDJEOWNYOWJkQ1wvNHBxZDZLZEZBcjRFcmdYWjZEZ2IrR0E9IiwgICJvIiA6ICJpT1MifQ==";
         // msg = "";
         // resultCode = 6666; 666代表成功
-//        new PhoneAuthenTool().startPhoneAuthen(phoneNum,promise);
+        new PhoneAuthenTool().startPhoneAuthen(phoneNum,promise);
         WritableMap map = Arguments.createMap();
         map.putInt("resultCode", 6666);
         promise.resolve(map);
