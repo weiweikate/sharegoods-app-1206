98627923f62879aec881877567f94342cd58d947
lymluck <894405031@qq.com>
2019-04-24 11:01:28 +0800

    feat：首页静默刷新

--- android/app/src/main/java/com/meeruu/sharegoods/event/Event.java
+++ android/app/src/main/java/com/meeruu/sharegoods/event/Event.java
@@ -3,5 +3,14 @@ package com.meeruu.sharegoods.event;
 public class Event {
 
     public static class MRHomeRefreshEvent {
+        private int homeType;
+
+        public MRHomeRefreshEvent(int homeType) {
+            this.homeType = homeType;
+        }
+
+        public int getHomeType() {
+            return homeType;
+        }
     }
 }
--- android/app/src/main/java/com/meeruu/sharegoods/receiver/MRJPushReceiver.java
+++ android/app/src/main/java/com/meeruu/sharegoods/receiver/MRJPushReceiver.java
@@ -46,8 +46,9 @@ public class MRJPushReceiver extends BroadcastReceiver {
             Log.d(TAG, "[MyReceiver] 接收Registration Id : " + regId);
         } else if (JPushInterface.ACTION_MESSAGE_RECEIVED.equals(intent.getAction())) {
             Log.d(TAG, "[MyReceiver] 接收到推送下来的自定义消息: " + bundle.getString(JPushInterface.EXTRA_MESSAGE));
-//            processCustomMessage(context, bundle);
-            receiveMsg(context, bundle.getString(JPushInterface.EXTRA_CONTENT_TYPE), objExtra);
+            String content = bundle.getString(JPushInterface.EXTRA_MESSAGE);
+            String type = bundle.getString(JPushInterface.EXTRA_CONTENT_TYPE);
+            receiveMsg(context, content, type, objExtra);
         } else if (JPushInterface.ACTION_NOTIFICATION_RECEIVED.equals(intent.getAction())) {
             Log.d(TAG, "[MyReceiver] 接收到推送下来的通知");
             int notifactionId = bundle.getInt(JPushInterface.EXTRA_NOTIFICATION_ID);
@@ -68,12 +69,16 @@ public class MRJPushReceiver extends BroadcastReceiver {
         }
     }
 
-    private void receiveMsg(Context context, String type, JSONObject objExtra) {
-        LogUtils.d("======" + type + "======" + objExtra.toString());
+    private void receiveMsg(Context context, String content, String type, JSONObject objExtra) {
+        LogUtils.d("======" + content + "======" + type + "======" + objExtra);
         switch (type) {
             case "HomeRefresh":
                 // 刷新首页
-                EventBus.getDefault().post(new Event.MRHomeRefreshEvent());
+                try {
+                    JSONObject object = new JSONObject(content);
+                    EventBus.getDefault().post(new Event.MRHomeRefreshEvent(object.getInt("homeType")));
+                } catch (JSONException e) {
+                }
                 break;
             default:
                 break;
