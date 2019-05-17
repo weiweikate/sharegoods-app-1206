3f5656a78ec7f52f8a1208c0fffb0cff6e254aba
lymluck <894405031@qq.com>
2019-05-17 18:22:07 +0800

    fix：修复神策匿名id问题

--- android/commonlib/src/main/java/com/meeruu/commonlib/service/InitializeService.java
+++ android/commonlib/src/main/java/com/meeruu/commonlib/service/InitializeService.java
@@ -83,7 +83,7 @@ public class InitializeService extends IntentService {
             // 一键登录debug
             JVerificationInterface.setDebugMode(true);
             // 神策
-            SensorsDataAPI.sharedInstance().enableLog(true);
+//            SensorsDataAPI.sharedInstance().enableLog(true);
         } else {
             JPushInterface.setDebugMode(false);
             JPushInterface.initCrashHandler(getApplicationContext());
