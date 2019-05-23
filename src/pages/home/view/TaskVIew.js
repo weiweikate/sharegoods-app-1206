c0118c4edccca4f0f932d49866f58e147c599542
胡超_iOS <huchao@meeruu.com>
2019-05-23 15:57:47 +0800

    删除shareTask

---
+++ src/pages/home/view/TaskVIew.js
@@ -0,0 +1,53 @@
+/**
+ *
+ * Copyright 2018 杭州飓热科技有限公司   版权所有
+ * Copyright 2018 JURE Group Holding Ltd. All Rights Reserved
+ *
+ * @flow
+ * @format
+ * Created by huchao on 2019/5/23.
+ *
+ */
+'use strict';
+
+import React from 'react';
+
+import {
+  StyleSheet,
+  View,
+  TouchableOpacity
+} from 'react-native';
+
+import {
+  UIText,
+  UIImage
+} from '../../../components/ui';
+import DesignRule from 'DesignRule';
+
+export default class TaskVIew extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this._bind();
+
+    this.state = {};
+  }
+
+  _bind() {
+
+  }
+
+  componentDidMount() {
+  }
+
+
+  render() {
+    return (
+      <View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({});
