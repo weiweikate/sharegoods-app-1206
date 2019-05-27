941f11f73bf175b1d0829cb850033741486edc69
胡玉峰 <huyufeng@meeruu.com>
2019-05-27 09:49:51 +0800

    fix：埋点更改

---
+++ src/utils/TrackEvent/PaymentPoint.js
@@ -0,0 +1,10 @@
+//    公用模块
+const PaymentPoint = {
+   "ViewOrderPayPage": {
+       'des': '浏览订单支付页面',
+       'name': 'ViewOrderPayPage',
+       'params': {}
+   }
+};
+
+export default PaymentPoint
