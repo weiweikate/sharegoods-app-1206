a54a0a9a63a641c457e0db0f1ae3b729e35a3a84
胡玉峰 <huyufeng@meeruu.com>
2019-05-28 18:46:44 +0800

    优化评论功能

--- ios/Class/Modules/Help/CommentTool.m
+++ ios/Class/Modules/Help/CommentTool.m
@@ -19,7 +19,6 @@ SINGLETON_FOR_CLASS(CommentTool)
 -(void)checkIsCanComment{
 //  [self showAlter];
 //  return;
-  
    NSString *savedTimeStamp = [[NSUserDefaults standardUserDefaults] objectForKey:LASTCOMMENT_TIME_STAMP];
    NSString *commentInterval = [[NSUserDefaults standardUserDefaults]objectForKey:COMMENT_INTERVAL];
   if (savedTimeStamp && commentInterval ) {
@@ -28,7 +27,7 @@ SINGLETON_FOR_CLASS(CommentTool)
     NSLog(@"保存的时间戳%ld,保存的时间间隔%ld",savedTimeStamp_integer,commentInterval_integer);
     if ((savedTimeStamp_integer + commentInterval_integer * 24 * 60) < [[self getNowTimeStamp] integerValue]) {
       //超过间隔时间可以弹出评论
-      [self showAlter];
+      [self showAlterWith:commentInterval_integer];
       [[NSUserDefaults standardUserDefaults]setObject:[self getNowTimeStamp] forKey:LASTCOMMENT_TIME_STAMP];
     }
   }else{
@@ -38,13 +37,18 @@ SINGLETON_FOR_CLASS(CommentTool)
   }
 }
 
--(void)showAlter{
+/**
+ @param timeInterval 间隔天数
+ */
+-(void)showAlterWith:(NSInteger)timeInterval{
   UIAlertController *alterController = [UIAlertController alertControllerWithTitle:@"温馨提示"
                                                                            message:@"亲爱的秀宝宝您已经用了一段时间小秀了，可否来个好评~"
                                                                     preferredStyle:UIAlertControllerStyleAlert];
   UIAlertAction *actionCancel = [UIAlertAction actionWithTitle:@"没时间"
                                                          style:UIAlertActionStyleDefault
-                                                       handler:^(UIAlertAction * _Nonnull action) { }];
+                                                       handler:^(UIAlertAction * _Nonnull action) {
+                                                         [self saveNewTimeInterval:timeInterval];
+                                                       }];
   
   UIAlertAction *actionSubmit = [UIAlertAction actionWithTitle:@"好嘞"
                                                          style:UIAlertActionStyleDefault
@@ -55,7 +59,6 @@ SINGLETON_FOR_CLASS(CommentTool)
   [alterController addAction:actionCancel];
   [alterController addAction:actionSubmit];
   [self.currentViewController_XG presentViewController:alterController animated:YES completion:^{}];
-  
 }
 -(void)goToAppStore{
   //itms-apps://itunes.apple.com/cn/app/id1439275146?mt=8&action=write-review
@@ -77,14 +80,15 @@ SINGLETON_FOR_CLASS(CommentTool)
     }
   }
 }
+/**
+ 获取当前时间戳
+
+ @return 时间戳String类型
+ */
 -(NSString *)getNowTimeStamp{
-  
   NSDate *datenow = [NSDate date];//现在时间,你可以输出来看下是什么格式
-  
   NSString *timeSp = [NSString stringWithFormat:@"%ld", (long)[datenow timeIntervalSince1970]];
-  
   return timeSp;
-  
 }
 
 @end
