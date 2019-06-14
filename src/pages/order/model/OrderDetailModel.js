68e3a863e337619a6499bce7efaa63371acec736
胡超_iOS <huchao@meeruu.com>
2019-06-14 18:03:28 +0800

    订单详情头部状态，倒计时

--- src/pages/order/model/OrderDetailModel.js
+++ src/pages/order/model/OrderDetailModel.js
@@ -10,10 +10,6 @@ export const orderStatus = {
     didPayment: 2
 }
 
-export const orderStatusMessage = {
-    [orderStatus.prePayment] : '等待买家付款',
-    [orderStatus.didPayment] : '买家已付款'
-}
 
 class OrderStatusModel {
     @observable status = 0
@@ -73,6 +69,7 @@ class OrderDetailModel {
         this.merchantOrder = data.merchantOrder || {}
         this.payInfo = data.payInfo || {}
         this.receiveInfo = data.receiveInfo || {}
+        let {payTime, nowTime, receiveTime, cancelReason} = this.baseInfo
 
         orderDetailModel.loadingState=PageLoadingState.success
         this.platformOrderNo = this.merchantOrder.platformOrderNo || '';
@@ -82,20 +79,34 @@ class OrderDetailModel {
             {
                 this.moreDetail = '';
                 this.sellerState = '';
-                this.buyState = '';
+                this.buyState = '等待买家付款';
+                if (payTime - nowTime > 0){
+                    this.startTimer((payTime - nowTime)/1000);
+                }
                 break;
             }
             case OrderType.WAIT_DELIVER:
             {
+                this.moreDetail = '';
+                this.sellerState = '';
+                this.buyState = '买家已付款';
                 break;
             }
             case OrderType.DELIVERED:
             {
-
+                this.moreDetail = '';
+                this.sellerState = '';
+                this.buyState = '平台已发货';
+                if (receiveTime - nowTime > 0){
+                    this.startTimer((payTime - nowTime)/1000);
+                }
                 break;
             }
             case OrderType.COMPLETED:
             {
+                this.moreDetail = '';
+                this.sellerState = '';
+                this.buyState = '订单完成';
                 if (this.merchantOrder.commentStatus){
                     menu.push({
                         id: 10,
@@ -107,6 +118,9 @@ class OrderDetailModel {
             }
             case OrderType.CLOSED:
             {
+                this.moreDetail = cancelReason;
+                this.sellerState = '';
+                this.buyState = '交易关闭';
                 break;
             }
             default:
@@ -116,6 +130,52 @@ class OrderDetailModel {
                 break;
         }
         this.menu = menu;
+    }
+
+    @action
+    stopTimer() {
+        this.timer && clearInterval(this.timer);
+    }
+
+    /**
+     * 获取剩余时间的字符串
+     * @param out_time 失效时间 number
+     * return 如果当前时间大于 out_time 返回 null
+     */
+    @action
+    getRemainingTime(remainingTime) {
+        if (remainingTime <= 0) {
+            return '已超时';
+        }
+        let s = remainingTime % 60;
+        remainingTime = (remainingTime - s) / 60;
+        let m = remainingTime % 60;
+        remainingTime = (remainingTime - m) / 60;
+        let H = remainingTime % 24;
+        remainingTime = (remainingTime - H) / 24;
+        let d = remainingTime;
+
+        return  d + '天' + H + '小时' + m + '分' + s + '秒';
+    }
+
+    @action
+    startTimer(remainingTime) {
+        this.stopTimer();
+        if (remainingTime === null || remainingTime === undefined) {
+            return;
+        }
+        /** 当前的时间已经超出，不开启定时器*/
+        this.moreDetail = this.getRemainingTime(remainingTime);
+        this.timer = setInterval(() => {
+            remainingTime--;
+            this.moreDetail = this.getRemainingTime(remainingTime);
+            if (this.moreDetail === '已超时') {
+                this.stopTimer();
+                this.loadDetailInfo(this.merchantOrderNo);
+            } else {
+
+            }
+        }, 1000);
     }
 
 
