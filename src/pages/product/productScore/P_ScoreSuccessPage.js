171fe6d37cbdd85db498973cace77d85d881d619
slardar_chen <chen_yangjun@yeah.net>
2019-06-27 09:55:44 +0800

    fix: replace

--- src/pages/product/productScore/P_ScoreSuccessPage.js
+++ src/pages/product/productScore/P_ScoreSuccessPage.js
@@ -40,7 +40,7 @@ export default class P_ScoreSuccessPage extends BasePage {
     }
 
     $NavBarLeftPressed = () => {
-        this.$navigateBack(2);
+        this.$navigateBack();
     };
 
     $navigationBarOptions = {
