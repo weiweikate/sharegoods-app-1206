39d3e205a7e533ef0ba3b83c41edb13c07c88942
胡超_iOS <huchao@meeruu.com>
2019-07-04 16:36:41 +0800

    1

--- src/pages/order/order/OrderType.js
+++ src/pages/order/order/OrderType.js
@@ -73,8 +73,8 @@ const ViewOrderStatus = {
 // 返回订单详情售后按钮list
 function GetAfterBtns(product) {
     if (product.status === OrderType.WAIT_PAY ||
-        product.status === OrderType.DELETED ||
-        product.status === OrderType.CLOSED
+        product.status === OrderType.DELETED 
+        // product.status === OrderType.CLOSED
     ) {
         return [];
     }
