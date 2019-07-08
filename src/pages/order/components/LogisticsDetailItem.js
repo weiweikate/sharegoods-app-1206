6b60b0d79fd1ef744d3eb865a331fe9de2af552a
zhoujianxin <zhoujianxin@meeruu.com>
2019-07-08 15:09:24 +0800

    物流

--- src/pages/order/components/LogisticsDetailItem.js
+++ src/pages/order/components/LogisticsDetailItem.js
@@ -18,7 +18,7 @@ const LogisticsDetailItem = props => {
 
     this.renderMiddleImage = () => {
         return (
-                <UIText value={'·'} style={{ fontSize: px2dp(40)}}/>
+                <View style={{width:6,height:6,borderRadius:3,backgroundColor:'black'}}/>
         );
     };
     this.renderMiddleLine = () => {
@@ -42,12 +42,17 @@ const LogisticsDetailItem = props => {
         <View style={{ flexDirection: 'row',paddingLeft: px2dp(15),paddingRight:px2dp(21),}}>
             <View style={{width: px2dp(40), paddingTop: px2dp(10),marginRight:px2dp(15)}}>
                 <UIText value={time.substr(5,5)} style={{ fontSize: px2dp(14), color: isTop ? DesignRule.textColor_mainTitle_222 : DesignRule.textColor_instruction,textAlign:'right',marginRight:-1}}/>
-            <UIText value={time.substr(10,6)} style={{ fontSize: px2dp(11), color: isTop ? DesignRule.textColor_mainTitle_222 : DesignRule.textColor_instruction,marginTop:px2dp(-14) ,textAlign:'right' }}/>
+                <UIText value={time.substr(10,6)} style={{ fontSize: px2dp(11), color: isTop ? DesignRule.textColor_mainTitle_222 : DesignRule.textColor_instruction,marginTop:px2dp(-14) ,textAlign:'right' }}/>
             </View>
             {this.renderMiddleLine()}
-            <View style={{ position: 'absolute', left:ScreenUtils.isIphoneMax ? px2dp(60) : px2dp(65), top: px2dp(5) }}>
-                {this.renderMiddleImage()}
-            </View>
+            {isTop ?
+                <View style={{position: 'relative', left: -3.5, top:!isBottom ? px2dp(32) : px2dp(26)}}>
+                    {this.renderMiddleImage()}
+                </View> :
+                <View style={{position: 'relative', left: -3.5, alignItems: 'center', justifyContent: 'center'}}>
+                    {this.renderMiddleImage()}
+                </View>
+            }
                 <View style={{ paddingLeft: px2dp(25),paddingRight:px2dp(48), flexWrap: 'wrap',paddingTop:px2dp(15) }}>
                     <UIText value={content1} style={{ fontSize: px2dp(13), color: isTop ? DesignRule.textColor_mainTitle_222 : DesignRule.textColor_instruction }}/>
             </View>
