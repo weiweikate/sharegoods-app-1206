9fdef585bd5b3f8c3163daa2f1c0eb40812d5d87
胡玉峰 <huyufeng@meeruu.com>
2019-07-09 09:49:13 +0800

    feat:增加不可单买图标

--- src/pages/shopCart/res/index.js
+++ src/pages/shopCart/res/index.js
@@ -3,6 +3,7 @@ const index = {
     kongShopCartImg: require('./gowuche_icon.png'),
     ZanWeiKaiShou:require('./common_icon_normal.png'),
     shopCartNoGoods:require('./shopCartNoGoods.png'),
+    shopCart_no_single_buy:require('./shopCart_no_single_buy.png'),
     ...res
 };
 export default index;
