600327e5828311692cdd8755b9ae7fd704895397
lymluck <894405031@qq.com>
2019-07-09 14:14:11 +0800

    fix：升级极光sdk

--- android/app/src/main/java/com/meeruu/sharegoods/receiver/MRJPushReceiver.java
+++ android/app/src/main/java/com/meeruu/sharegoods/receiver/MRJPushReceiver.java
@@ -1,10 +1,6 @@
 package com.meeruu.sharegoods.receiver;
 
-import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.content.Intent;
-import android.os.Bundle;
-import android.util.Log;
 
 import com.meeruu.commonlib.utils.AppUtils;
 import com.meeruu.commonlib.utils.LogUtils;
@@ -15,7 +11,9 @@ import org.greenrobot.eventbus.EventBus;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import cn.jpush.android.api.JPushInterface;
+import cn.jpush.android.api.CustomMessage;
+import cn.jpush.android.api.NotificationMessage;
+import cn.jpush.android.service.JPushMessageReceiver;
 
 /**
  * 自定义接收器
@@ -24,50 +22,51 @@ import cn.jpush.android.api.JPushInterface;
  * 1) 默认用户会打开主界面
  * 2) 接收不到自定义消息
  */
-public class MRJPushReceiver extends BroadcastReceiver {
+public class MRJPushReceiver extends JPushMessageReceiver {
 
-    private static final String TAG = "JPush";
     private static final String PACKAGENAME = "com.meeruu.sharegoods";
 
+    // 注册回调
     @Override
-    public void onReceive(final Context context, Intent intent) {
-        Bundle bundle = intent.getExtras();
-        JSONObject objExtra = null;
+    public void onRegister(Context context, String regId) {
+        super.onRegister(context, regId);
+        // 将推送 ID 保存到用户表中
+        SensorsDataAPI.sharedInstance().profilePushId("jgId", regId);
+    }
+
+    // 通知被打开
+    @Override
+    public void onNotifyMessageOpened(Context context, NotificationMessage notificationMessage) {
+        super.onNotifyMessageOpened(context, notificationMessage);
         try {
-            String extra = bundle.getString(JPushInterface.EXTRA_EXTRA);
-            if (extra != null) {
-                objExtra = new JSONObject(extra);
-            }
-        } catch (JSONException e) {
-        }
-
-        if (JPushInterface.ACTION_REGISTRATION_ID.equals(intent.getAction())) {
-            final String regId = bundle.getString(JPushInterface.EXTRA_REGISTRATION_ID);
-            // 将推送 ID 保存到用户表中
-            SensorsDataAPI.sharedInstance().profilePushId("jgId", regId);
-            Log.d(TAG, "[MyReceiver] 接收Registration Id : " + regId);
-        } else if (JPushInterface.ACTION_MESSAGE_RECEIVED.equals(intent.getAction())) {
-            Log.d(TAG, "[MyReceiver] 接收到推送下来的自定义消息: " + bundle.getString(JPushInterface.EXTRA_MESSAGE));
-            String content = bundle.getString(JPushInterface.EXTRA_MESSAGE);
-            String type = bundle.getString(JPushInterface.EXTRA_CONTENT_TYPE);
-            receiveMsg(context, content, type, objExtra);
-        } else if (JPushInterface.ACTION_NOTIFICATION_RECEIVED.equals(intent.getAction())) {
-            Log.d(TAG, "[MyReceiver] 接收到推送下来的通知" + bundle.toString());
-            int notifactionId = bundle.getInt(JPushInterface.EXTRA_NOTIFICATION_ID);
-            Log.d(TAG, "[MyReceiver] 接收到推送下来的通知的ID: " + notifactionId);
-            receiveNotify(objExtra);
-        } else if (JPushInterface.ACTION_NOTIFICATION_OPENED.equals(intent.getAction())) {
-            Log.d(TAG, "[MyReceiver] 用户点击打开了通知");
+            JSONObject objExtra = new JSONObject(notificationMessage.notificationExtras);
             notifyOpened(context, objExtra);
-        } else if (JPushInterface.ACTION_RICHPUSH_CALLBACK.equals(intent.getAction())) {
-            Log.d(TAG, "[MyReceiver] 用户收到到RICH PUSH CALLBACK: " + bundle.getString(JPushInterface.EXTRA_EXTRA));
-            //在这里根据 JPushInterface.EXTRA_EXTRA 的内容处理代码，比如打开新的Activity， 打开一个网页等..
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
 
-        } else if (JPushInterface.ACTION_CONNECTION_CHANGE.equals(intent.getAction())) {
-            boolean connected = intent.getBooleanExtra(JPushInterface.EXTRA_CONNECTION_CHANGE, false);
-            Log.w(TAG, "[MyReceiver]" + intent.getAction() + " connected state change to " + connected);
-        } else {
-            Log.d(TAG, "[MyReceiver] Unhandled intent - " + intent.getAction());
+    // 自定义消息
+    @Override
+    public void onMessage(Context context, CustomMessage customMessage) {
+        super.onMessage(context, customMessage);
+        try {
+            JSONObject objExtra = new JSONObject(customMessage.extra);
+            receiveMsg(context, customMessage.message, customMessage.contentType, objExtra);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // 通知消息
+    @Override
+    public void onNotifyMessageArrived(Context context, NotificationMessage notificationMessage) {
+        super.onNotifyMessageArrived(context, notificationMessage);
+        try {
+            JSONObject objExtra = new JSONObject(notificationMessage.notificationExtras);
+            receiveNotify(objExtra);
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
     }
 
@@ -98,11 +97,6 @@ public class MRJPushReceiver extends BroadcastReceiver {
 
     //接收到通知
     private void receiveNotify(JSONObject objExtra) {
-        try {
-            LogUtils.d("======" + objExtra.getString("linkUrl"));
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
     }
 
     //用户点击了通知
