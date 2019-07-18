72f185ed5f74c4aec2451a52608838de460e34c6
胡超_iOS <huchao@meeruu.com>
2019-07-18 10:37:17 +0800

    修复bug

--- src/pages/order/order/ConfirmOrderPage.js
+++ src/pages/order/order/ConfirmOrderPage.js
@@ -144,7 +144,7 @@ export default class ConfirmOrderPage extends BasePage {
 
     // 地址重新选择
     selectAddress = () => {
-        if (confirmOrderModel.isNoAddress){
+        if (confirmOrderModel.isNoAddress === false){
             this.$navigate('mine/address/AddressManagerPage', {
                 from: 'order',
                 currentId: confirmOrderModel.addressId,
