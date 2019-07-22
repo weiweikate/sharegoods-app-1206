5f3ebe10f13b26298004879251e2696af1e6bd33
lymluck <894405031@qq.com>
2019-07-22 10:38:12 +0800

    fix：优惠券样式优化

--- src/App.js
+++ src/App.js
@@ -56,22 +56,7 @@ if (__DEV__) {
         'waiting:',
         waitingModuleNames.length
     );
-} else {
-    // 非开发环境，屏蔽所有console
-    global.console = {
-        info: () => {
-        },
-        log: () => {
-        },
-        warn: () => {
-        },
-        debug: () => {
-        },
-        error: () => {
-        }
-    };
 }
-
 
 let codePushOptions = {
     checkFrequency: codePush.CheckFrequency.ON_APP_RESUME
@@ -150,7 +135,6 @@ class App extends Component {
                 ScreenUtils.checkhasNotchScreen((data) => {
                     ScreenUtils.setHasNotchScreen(data);
                 });
-
             }, 3000);
         });
         this.listenerJSMessage = JSManagerEmitter.addListener('MINE_NATIVE_TO_RN_MSG', this.mineMessageData);
