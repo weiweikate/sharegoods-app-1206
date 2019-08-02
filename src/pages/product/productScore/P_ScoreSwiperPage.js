d2d594eddf69fad9c497c482f80b261038156090
slardar_chen <chen_yangjun@yeah.net>
2019-08-02 14:03:03 +0800

    fix: 更换图片查看

--- src/pages/product/productScore/P_ScoreSwiperPage.js
+++ src/pages/product/productScore/P_ScoreSwiperPage.js
@@ -1,19 +1,13 @@
 import React from 'react';
-import { View, StyleSheet, Image } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import BasePage from '../../../BasePage';
 // import VideoView from '../../../components/ui/video/VideoView';
 import UIImage from '@mr/image-placeholder';
 import ScreenUtils from '../../../utils/ScreenUtils';
-// import StringUtils from '../../../utils/StringUtils';
 import { MRText as Text } from '../../../components/ui/UIText';
-// import DesignRule from '../../../constants/DesignRule';
-import NoMoreClick from '../../../components/ui/NoMoreClick';
-import pRes from '../res/product';
 import VideoView from '../../../components/ui/video/VideoView';
 import DesignRule from '../../../constants/DesignRule';
-import MRBannerView from '../../../components/ui/bannerView/MRBannerView';
-
-const { swiper_cancel } = pRes.productScore;
+import FlyImageViewer from '../../../comm/components/FlyImageViewer';
 
 
 const { width, height } = ScreenUtils;
@@ -38,12 +32,6 @@ export default class P_ScoreSwiperPage extends BasePage {
         }
     };
 
-    componentDidMount() {
-        setTimeout(() => {
-            this.MRBannerView.scrollToIndexWithAnimate(this.params.index, false);
-        }, 0);
-    }
-
     _render() {
         const { images, content } = this.params;
 
@@ -54,66 +42,31 @@ export default class P_ScoreSwiperPage extends BasePage {
         // }
         return (
             <View style={styles.containerView}>
-                <MRBannerView style={{ height: ScreenUtils.height, width: ScreenUtils.width + 0.5 }}
-                              ref={(ref) => {
-                                  this.MRBannerView = ref;
-                              }}
-                              resizeMode='contain'
-                              itemWidth={ScreenUtils.width + 0.5}
-                              imgUrlArray={this.videoImageList}
-                              itemSpace={0}
-                              onDidScrollToIndex={(e) => {
-                                  let index = e.nativeEvent.index;
-                                  this.setState({
-                                      index: index + 1
-                                  });
-                              }}
-                              autoLoop={false}
-                />
-                <View style={{
-                    position: 'absolute',
-                    alignSelf: 'center',
-                    height: 44,
-                    top: ScreenUtils.statusBarHeight,
-                    justifyContent: 'center'
-                }}>
-                    <Text style={{
-                        fontSize: 13,
-                        color: DesignRule.textColor_instruction
-                    }}>{`${this.state.index}/${this.videoImageList.length}`}</Text>
-                </View>
 
-                <View style={{
-                    position: 'absolute',
-                    backgroundColor: 'rgba(0,0,0,0.5)',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    height: 48,
-                    width: width,
-                    bottom: 0, left: 0, right: 0
-                }}>
-                    <Text style={{
-                        fontSize: 12,
-                        marginHorizontal: 15,
-                        color: DesignRule.white
-                    }}
-                          numberOfLines={3}>{content}</Text>
+                <FlyImageViewer imageUrls={this.videoImageList}
+                                index={this.params.index}
+                                unShowDown={true}
+                                onCancel={() => {
+                                    this.props.navigation.goBack();
+                                }}>
+                    <View style={{
+                        position: 'absolute',
+                        backgroundColor: 'rgba(0,0,0,0.5)',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        height: 48,
+                        width: width,
+                        bottom: 0, left: 0, right: 0
+                    }}>
+                        <Text style={{
+                            fontSize: 12,
+                            marginHorizontal: 15,
+                            color: DesignRule.white
+                        }}
+                              numberOfLines={3}>{content}</Text>
 
-                </View>
-
-                <NoMoreClick style={{
-                    position: 'absolute',
-                    width: 44,
-                    height: 44,
-                    top: ScreenUtils.statusBarHeight,
-                    left: 14,
-                    justifyContent: 'center',
-                    alignItems: 'center'
-                }} onPress={() => {
-                    this.$navigateBack();
-                }}>
-                    <Image source={swiper_cancel}/>
-                </NoMoreClick>
+                    </View>
+                </FlyImageViewer>
             </View>
         );
     }
