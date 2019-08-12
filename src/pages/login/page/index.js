448497bee16771bd81be8fd238bd13714e0f1f0f
lymluck <894405031@qq.com>
2019-08-12 17:52:14 +0800

    fix：修复登录bug

--- src/pages/login/page/index.js
+++ src/pages/login/page/index.js
@@ -1,39 +1,22 @@
-import OtherLoginPage from './LoginPage';
-import LoginPage from '../view/LoginPage';
-import InputPhoneNum from './InputPhoneNum';
-import InputCode from './InputCode';
+import LoginPage from './LoginPage';
 
-import SetPasswordPage from './SetPasswordPage';
-import RegistPage from './RegistPage';
 import ForgetPasswordPage from './ForgetPasswordPage';
-import GetRedpacketPage from './GetRedpacketPage';
 import InviteCodePage from './InviteCodePage';
 import SelectMentorPage from './SelectMentorPage';
 import MentorDetailPage from './MentorDetailPage';
-import LocalNumLogin from './LocalNumLogin';
-import LoginView from '../view/LoginPage';
-import PwdLoginPage from '../view/PwdLoginPage';
-import PhoneLoginPage from '../view/PhoneLoginPage';
-import LoginVerifyCodePage from '../view/LoginVerifyCodePage';
+import PwdLoginPage from './PwdLoginPage';
+import PhoneLoginPage from './PhoneLoginPage';
+import LoginVerifyCodePage from './LoginVerifyCodePage';
 
 // 访问路径 'Login/Demo1'
 export default {
     moduleName: 'login',    //模块名称
     childRoutes: { //模块内部子路由
-        InputCode,
-        InputPhoneNum,
-        LocalNumLogin,
-        OtherLoginPage,
-
         LoginPage,
-        SetPasswordPage,
-        RegistPage,
         ForgetPasswordPage,
-        GetRedpacketPage,
         InviteCodePage,
         SelectMentorPage,
         MentorDetailPage,
-        LoginView,
         PhoneLoginPage,
         PwdLoginPage,
         LoginVerifyCodePage
