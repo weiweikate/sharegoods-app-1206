e48d49d1665695c6ab55c7482157fda57201ebef
ziming.x <xieziming@meeruu.com>
2019-09-11 15:20:30 +0800

    下拉UI

--- src/comm/components/lottieheader/ListHeaderLoading.js
+++ src/comm/components/lottieheader/ListHeaderLoading.js
@@ -2,16 +2,17 @@
 import React from 'react';
 import RefreshAnimateHeader from './RefreshAnimateHeader';
 import DesignRule from '../../../constants/DesignRule';
+import {Platform} from 'react-native';
 
 function ListHeaderLoading(props) {
-  const { isRefreshing, onRefresh,source, ...others } = props;
+  const { isRefreshing, onRefresh, ...others } = props;
   const headerHeight = DesignRule.width/750*120;
-
+  const source = Platform.OS !== 'ios' ?  require('./pull3.json') :  require('./pull_step2.json')
   return (
     <RefreshAnimateHeader
       refreshing={isRefreshing}
       headerHeight={headerHeight || 70}
-      source={source || require('./pull3.json')}
+      source={source}
       onRefresh={onRefresh}
       {...others}
     />
