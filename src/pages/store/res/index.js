69d8d0e6e7b152648ed452a9f1ada1808a8768b5
slardar_chen <chen_yangjun@yeah.net>
2019-09-17 15:50:08 +0800

    feat: 图片

---
+++ src/pages/store/res/index.js
@@ -0,0 +1,33 @@
+import res from '../../../comm/res';
+import shopSetting from './shopSetting';
+import shopRecruit from './shopRecruit';
+import recommendSearch from './recommendSearch';
+import openShop from './openShop';
+import myShop from './myShop';
+import addCapacity from './addCapacity';
+
+const index = {
+    jbtk_03: require('./jbtk_03.png'),
+    pindianzhaojiling: require('./pindianzhaojiling.png'),
+    pindianzhaojilingbg: require('./pindianzhaojilingbg.webp'),
+    shopSetting: {
+        ...shopSetting
+    },
+    shopRecruit: {
+        ...shopRecruit
+    },
+    recommendSearch: {
+        ...recommendSearch
+    },
+    openShop: {
+        ...openShop
+    },
+    myShop: {
+        ...myShop
+    },
+    addCapacity: {
+        ...addCapacity
+    },
+    ...res
+};
+export default index;
