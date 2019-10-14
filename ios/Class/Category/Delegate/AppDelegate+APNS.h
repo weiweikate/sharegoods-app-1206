bd328ff1277aabb19735735504a5fbd9d407cf98
huchao <huchao@meeruu.com>
2019-10-14 15:48:24 +0800

    1.修复iOS13，推送收不到bug。 2. Dark模式-》light模式

--- ios/Class/Category/Delegate/AppDelegate+APNS.h
+++ ios/Class/Category/Delegate/AppDelegate+APNS.h
@@ -7,10 +7,11 @@
 //
 
 #import "AppDelegate.h"
+// 引入 JPush 功能所需头文件
+#import "JPUSHService.h"
 
 
-
-@interface AppDelegate (APNS)
+@interface AppDelegate (APNS) <JPUSHRegisterDelegate>
 -(void)JR_ConfigAPNS:(UIApplication *)application  didFinishLaunchingWithOptions:(NSDictionary *)launchOptions;
 
 @end
